ae9a570b98d07270084add253c75f2261b8e0c48
twatson83 <tswatson123@gmail.com>
2014-03-21 09:21:12 +0000

    Added comments to Bus and Configuration

--- R.MessageBus.Interfaces/IBus.cs
+++ R.MessageBus.Interfaces/IBus.cs
@@ -1,8 +1,23 @@
+using System;
+
 namespace R.MessageBus.Interfaces
 {
-    public interface IBus
-    {
+    public interface IBus : IDisposable
+    {   
+        /// <summary>
+        /// Contains the Bus configuration.
+        /// </summary>
         IConfiguration Configuration { get; set; }
-        void StartConsuming(string configPath, string endPoint, string queue = null);
+
+        /// <summary>
+        /// Sets up the Bus to start consuming messages on the given queue.
+        /// </summary>
+        /// <param name="queue">The name of the queue to consume messages on.</param>
+        void StartConsuming(string queue = null);
+
+        /// <summary>
+        /// Stop consuming messages.
+        /// </summary>
+        void StopConsuming();
     }
 }
