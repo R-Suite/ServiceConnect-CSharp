42a2e7a2ecb85704dd776230a3002fc64269cdc1
twatson83 <tswatson123@gmail.com>
2014-03-28 14:03:08 +0000

    Added Burger project

---
+++ samples/McDonalds/MacDonalds.Burgers/Program.cs
@@ -0,0 +1,12 @@
+﻿using R.MessageBus;
+
+namespace MacDonalds.Burgers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var bus = Bus.Initialize();
+        }
+    }
+}
