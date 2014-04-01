d1dc997fa91a4d8db0960862c840368c17db26b0
jpachansky <jpachansky@gmail.com>
2014-04-01 14:29:42 +0100

    Add simple batch file for publishing NuGet packages
    This is a temporary solution to get the "nuget thing" going.

---
+++ src/Tools/packaging/BuildNugetBuildPackages.bat
@@ -0,0 +1,4 @@
+--START cmd.exe /k "nuget pack C:\GitHub\R.MessageBus\src\R.MessageBus.Interfaces\R.MessageBus.Interfaces.nuspec"
+START nuget pack "C:\GitHub\R.MessageBus\src\R.MessageBus.Client.RabbitMQ\R.MessageBus.Client.RabbitMQ.nuspec"
+START nuget pack "C:\GitHub\R.MessageBus\src\R.MessageBus\R.MessageBus.nuspec"
+
