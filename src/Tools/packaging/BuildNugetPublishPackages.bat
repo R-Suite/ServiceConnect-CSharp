a50d5d4231139919a05176b738bdadca1a6c5d48
jpachansky <jpachansky@gmail.com>
2014-04-02 15:17:58 +0100

    Update nuspec for R.MessageBus

--- src/Tools/packaging/BuildNugetPublishPackages.bat
+++ src/Tools/packaging/BuildNugetPublishPackages.bat
@@ -11,9 +11,7 @@ NuGet pack "%OUTDIR%R.MessageBus\R.MessageBus.nuspec"
 NuGet pack "%OUTDIR%R.MessageBus.Interfaces\R.MessageBus.Interfaces.nuspec"
 
 
-nuget push R.MessageBus.0.0.0.4.nupkg
-nuget push R.MessageBus.Client.RabbitMQ.0.0.0.4.nupkg
-nuget push R.MessageBus.Interfaces.0.0.0.4.nupkg
+nuget push R.MessageBus.0.0.0.5.nupkg
            
 @ECHO === === === === === === === ===
 
