f9d845ef6bf87f5046ee917205a3c1fc57afa705
jpachansky <jpachansky@gmail.com>
2014-04-04 17:37:53 +0100

    Add sample project for Retries

---
+++ samples/Retries/Retries.Consumer/RetryMessageHandler.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using R.MessageBus.Interfaces;
+using Retries.Messages;
+
+namespace Retries.Consumer
+{
+    public class RetryMessageHandler : IMessageHandler<RetryMessage>
+    {
+        public void Execute(RetryMessage message)
+        {
+            Console.WriteLine("Handling message - {0}", message.CorrelationId);
+
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ samples/Retries/Retries.Producer/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using R.MessageBus;
+using Retries.Messages;
+
+namespace Retries.Producer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*********** Producer ***********");
+            var bus = Bus.Initialize(config => config.AddEndPointMapping(typeof(RetryMessage), "RetryTest"));
+
+            while (true)
+            {
+                Console.WriteLine("Press enter to send message");
+                Console.ReadLine();
+
+                var id = Guid.NewGuid();
+                bus.Send(new RetryMessage(id));
+
+                Console.WriteLine("Sent message - {0}", id);
+                Console.WriteLine("");
+            }
+        }
+    }
+}
