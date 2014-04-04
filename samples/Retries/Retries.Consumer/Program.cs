ca69b12414f45cbf8d2134054f5514e4988b91ab
jpachansky <jpachansky@gmail.com>
2014-04-04 17:37:53 +0100

    Add sample project for Retries

---
+++ samples/Retries/Retries.Consumer/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using R.MessageBus;
+
+namespace Retries.Consumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*********** Consumer ***********");
+            var bus = Bus.Initialize(x =>
+            {
+                x.ScanForMesssageHandlers = true;
+                x.SetQueueName("RetryTest");
+            });
+
+            bus.StartConsuming();
+
+            Console.ReadLine();
+        }
+    }
+}
