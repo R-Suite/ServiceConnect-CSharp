26dc47177447a2c704d83ff34acfcf50b784ccd5
jpachansky <jpachansky@gmail.com>
2014-04-25 12:16:15 +0100

    Publish updated nuget packages
    Updated sample Retries project with the latest nuget packages.

--- src/Tools/packaging/BuildNugetPublishPackages.bat
+++ src/Tools/packaging/BuildNugetPublishPackages.bat
@@ -11,7 +11,9 @@ NuGet pack "%OUTDIR%R.MessageBus\R.MessageBus.nuspec"
 NuGet pack "%OUTDIR%R.MessageBus.Interfaces\R.MessageBus.Interfaces.nuspec"
 
 
-nuget push R.MessageBus.0.0.0.5.nupkg
+nuget push R.MessageBus.0.0.0.7.nupkg
+nuget push R.MessageBus.Client.RabbitMQ.0.0.0.6.nupkg
+nuget push R.MessageBus.Interfaces.0.0.0.6.nupkg
            
 @ECHO === === === === === === === ===
 
