4cbd691e15c6c91ccafe86d80d0b9247dcee8a6a
jpachansky <jpachansky@gmail.com>
2014-05-19 10:49:50 +0100

    Add default parameter to pass messages headers

--- src/R.MessageBus.Interfaces/IProducer.cs
+++ src/R.MessageBus.Interfaces/IProducer.cs
@@ -1,10 +1,12 @@
+using System.Collections.Generic;
+
 namespace R.MessageBus.Interfaces
 {
     public interface IProducer
     {
-        void Publish<T>(T message) where T : Message;
-        void Send<T>(T message) where T : Message;
-        void Send<T>(string endPoint, T message) where T : Message;
+        void Publish<T>(T message, Dictionary<string, object> headers = null) where T : Message;
+        void Send<T>(T message, Dictionary<string, object> headers = null) where T : Message;
+        void Send<T>(string endPoint, T message, Dictionary<string, object> headers = null) where T : Message;
         void Disconnect();
         void Dispose();
     }
