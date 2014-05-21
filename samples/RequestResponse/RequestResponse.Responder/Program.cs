c3575b2d0297b4a89e506222fb45f7a2f69bd338
jpachansky <jpachansky@gmail.com>
2014-05-21 14:07:24 +0100

    Add sample project RequestResponse

---
+++ samples/RequestResponse/RequestResponse.Responder/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using R.MessageBus;
+
+namespace RequestResponse.Responder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*********** Responder ***********");
+
+            var bus = Bus.Initialize(x =>
+            {
+                x.ScanForMesssageHandlers = true;
+                x.SetQueueName("Responder");
+            });
+
+            bus.StartConsuming();
+
+            Console.ReadLine();
+        }
+    }
+}
