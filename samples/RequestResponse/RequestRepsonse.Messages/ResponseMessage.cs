b1cd8ca5751228f5d25c01c5616c4fefcb0c4368
jpachansky <jpachansky@gmail.com>
2014-05-21 14:07:24 +0100

    Add sample project RequestResponse

---
+++ samples/RequestResponse/RequestRepsonse.Messages/ResponseMessage.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using R.MessageBus.Interfaces;
+
+namespace RequestRepsonse.Messages
+{
+    public class ResponseMessage : Message
+    {
+        public ResponseMessage(Guid correlationId) : base(correlationId)
+        {
+        }
+    }
+}
---
+++ samples/RequestResponse/RequestResponse.Responder/RequestMessageHandler.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using R.MessageBus.Interfaces;
+using RequestRepsonse.Messages;
+
+namespace RequestResponse.Responder
+{
+    public class RequestMessageHandler : IMessageHandler<RequestMessage>
+    {
+        private readonly IBus _bus;
+
+        public RequestMessageHandler(IBus bus)
+        {
+            _bus = bus;
+        }
+
+        public void Execute(RequestMessage message)
+        {
+            var id = Guid.NewGuid();
+
+            //todo: this will need to change to use Bus.Reply()
+            _bus.Send("Requestor", new ResponseMessage(id));
+        }
+    }
+}
