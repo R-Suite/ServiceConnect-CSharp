ebf4962e8d9d5822c1614343ce628d3591dda916
jpachansky <jpachansky@gmail.com>
2014-05-21 14:07:24 +0100

    Add sample project RequestResponse

---
+++ samples/RequestResponse/RequestRepsonse.Messages/RequestMessage.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using R.MessageBus.Interfaces;
+
+namespace RequestRepsonse.Messages
+{
+    public class RequestMessage : Message
+    {
+        public RequestMessage(Guid correlationId) : base(correlationId)
+        {
+        }
+    }
+}
---
+++ samples/RequestResponse/RequestResponse.Requestor/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using R.MessageBus;
+using RequestRepsonse.Messages;
+
+namespace RequestResponse.Requestor
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("*********** Requestor ***********");
+
+            var bus = Bus.Initialize(config => config.SetQueueName("Requestor"));
+
+            while (true)
+            {
+                Console.WriteLine("Press enter to send message");
+                Console.ReadLine();
+
+                var id = Guid.NewGuid();
+                var result = bus.SendRequest<RequestMessage, ResponseMessage>("Responder", new RequestMessage(id));
+
+                Console.WriteLine("Sent message - {0}", id);
+                Console.WriteLine("");
+            }
+        }
+    }
+}
