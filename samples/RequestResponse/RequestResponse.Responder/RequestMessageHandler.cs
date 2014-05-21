3d434b95a6ce7fff175bfa3ccac33edab427ef8c
twatson83 <tswatson123@gmail.com>
2014-05-21 14:26:45 +0100

    When calling Bus.StartConsuming we should be filtering out any handlers that have a generic type for the message

--- samples/RequestResponse/RequestResponse.Responder/RequestMessageHandler.cs
+++ samples/RequestResponse/RequestResponse.Responder/RequestMessageHandler.cs
@@ -1,24 +1,15 @@
-﻿using System;
-using R.MessageBus.Interfaces;
+﻿using R.MessageBus.Interfaces;
 using RequestRepsonse.Messages;
 
 namespace RequestResponse.Responder
 {
     public class RequestMessageHandler : IMessageHandler<RequestMessage>
     {
-        private readonly IBus _bus;
-
-        public RequestMessageHandler(IBus bus)
-        {
-            _bus = bus;
-        }
+        public IConsumeContext Context { get; set; }
 
         public void Execute(RequestMessage message)
         {
-            var id = Guid.NewGuid();
-
-            //todo: this will need to change to use Bus.Reply()
-            _bus.Send("Requestor", new ResponseMessage(id));
+            Context.Reply(new ResponseMessage(message.CorrelationId));
         }
     }
 }
