bfe27b96f977577c53d6836b3a83dabbc89ddc1f
jpachansky <jpachansky@gmail.com>
2014-05-29 15:42:11 +0100

    Update sample project

--- src/Tools/packaging/BuildNugetPublishPackages.bat
+++ src/Tools/packaging/BuildNugetPublishPackages.bat
@@ -6,14 +6,14 @@ SET OUTDIR=C:\GitHub\R.MessageBus\src\
 
 del *.nupkg
 
-NuGet pack "%OUTDIR%R.MessageBus.Client.RabbitMQ\R.MessageBus.Client.RabbitMQ.nuspec"
-:: NuGet pack "%OUTDIR%R.MessageBus\R.MessageBus.nuspec"
-NuGet pack "%OUTDIR%R.MessageBus.Interfaces\R.MessageBus.Interfaces.nuspec"
+:: NuGet pack "%OUTDIR%R.MessageBus.Client.RabbitMQ\R.MessageBus.Client.RabbitMQ.nuspec"
+NuGet pack "%OUTDIR%R.MessageBus\R.MessageBus.nuspec"
+:: NuGet pack "%OUTDIR%R.MessageBus.Interfaces\R.MessageBus.Interfaces.nuspec"
 
 
-:: nuget push R.MessageBus.0.0.0.12.nupkg
-nuget push R.MessageBus.Interfaces.0.0.0.11.nupkg
-nuget push R.MessageBus.Client.RabbitMQ.0.0.0.11.nupkg
+nuget push R.MessageBus.1.0.0.12.nupkg
+:: nuget push R.MessageBus.Interfaces.1.0.0.11.nupkg
+:: nuget push R.MessageBus.Client.RabbitMQ.1.0.0.11.nupkg
            
 @ECHO === === === === === === === ===
 
