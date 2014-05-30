853ccd071d3c0195aafcf139e4049f0d85bf4cdd
jpachansky <jpachansky@gmail.com>
2014-05-30 10:47:55 +0100

    Add basic frame for SqlServer Persister

---
+++ src/R.MessageBus.IntegrationTests/SqlServerProcessManagerFinderTest.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Driver.Builders;
+using R.MessageBus.Interfaces;
+using R.MessageBus.Persistance.MongoDb;
+using R.MessageBus.Persistance.SqlServer;
+using Xunit;
+
+namespace R.MessageBus.IntegrationTests
+{
+    public class TestSqlServerData : IProcessManagerData
+    {
+        public Guid CorrelationId { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class SqlServerProcessManagerFinderTest
+    {
+        readonly Guid _correlationId = Guid.NewGuid();
+        private readonly string _connectionString;
+
+        public SqlServerProcessManagerFinderTest()
+        {
+            _connectionString = "Server=localhost;Database=ProcessManagerRepository;Trusted_Connection=True;";
+        }
+
+        [Fact]
+        public void ShouldInsertData()
+        {
+            // Arrange
+            IProcessManagerData data = new TestSqlServerData { CorrelationId = _correlationId, Name = "TestData" };
+            IProcessManagerFinder processManagerFinder = new SqlServerProcessManagerFinder(_connectionString);
+
+            // Act
+            processManagerFinder.InsertData(data);
+
+            // Assert
+            //todo
+        }
+    }
+}
