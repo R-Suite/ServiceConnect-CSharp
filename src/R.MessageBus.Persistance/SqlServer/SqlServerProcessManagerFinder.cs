90d1bf7eca554f1615fe1e69befc87dbf0fb6d3f
jpachansky <jpachansky@gmail.com>
2014-06-03 10:41:22 +0100

    Implement FindData and InsertData in SqlServerProcessManagerFinder
    Adding relevant integration test - using LocalDb instance.

--- src/R.MessageBus.Persistance/SqlServer/SqlServerProcessManagerFinder.cs
+++ src/R.MessageBus.Persistance/SqlServer/SqlServerProcessManagerFinder.cs
@@ -5,6 +5,9 @@ using R.MessageBus.Interfaces;
 
 namespace R.MessageBus.Persistance.SqlServer
 {
+    /// <summary>
+    /// Sql Server implementation of IProcessManagerFinder.
+    /// </summary>
     public class SqlServerProcessManagerFinder : IProcessManagerFinder
     {
         private readonly string _connectionString;
@@ -14,34 +17,75 @@ namespace R.MessageBus.Persistance.SqlServer
             _connectionString = connectionString;
         }
 
+        /// <summary>
+        /// Find existing instance of ProcessManager
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="id"></param>
+        /// <returns></returns>
         public IPersistanceData<T> FindData<T>(Guid id) where T : class, IProcessManagerData
         {
-            throw new NotImplementedException();
+            SqlServerData<T> result;
+
+            string tableName = typeof(T).Name;
+
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
+                using (var command = new SqlCommand())
+                {
+                    command.Connection = connection;
+                    command.CommandText = string.Format(@"SELECT * FROM {0} WHERE Id = @Id", tableName);
+                    command.Parameters.Add(new SqlParameter {ParameterName = "@Id", Value = id});
+                    var reader = command.ExecuteReader(CommandBehavior.SingleResult);
+                    reader.Read();
+
+                    var data = Newtonsoft.Json.JsonConvert.DeserializeObject<T>(reader["DataJson"].ToString());
+
+                    result = new SqlServerData<T> { Id = (Guid)reader["Id"], Data = data };
+                }
+            }
+
+            return result;
         }
 
+        /// <summary>
+        /// Create new instance of ProcessManager
+        /// When multiple threads try to create new ProcessManager instance, only the first one is allowed. 
+        /// All subsequent threads will update data instead.
+        /// </summary>
+        /// <param name="data"></param>
         public void InsertData(IProcessManagerData data)
         {
-            using(var sqlConnection=new SqlConnection(_connectionString))
+            string tableName = GetTableName(data);
+
+            using(var sqlConnection = new SqlConnection(_connectionString))
             {
-                string tableName = GetTableName(data);
-
-                // todo: create table if it doesn't exist
-
                 var sqlServerData = new SqlServerData<IProcessManagerData>
                 {
                     Data = data,
-                    //Version = 1,
-                    //Id = Guid.NewGuid()
+                    Id = data.CorrelationId
                 };
 
-                // todo: insert if doesn't exist, else update (only the first one is allowed)
-                string insertSql =
-                    string.Format("INSERT into {0} (CorrelationId,userID,idDepartment) VALUES (@staffName,@userID,@idDepartment)", tableName);
+                var sqlServerDataJson = Newtonsoft.Json.JsonConvert.SerializeObject(sqlServerData);
 
-                using (var command = new SqlCommand(insertSql))
+                // Insert if doesn't exist, else update (only the first one is allowed)
+                string upsertSql = string.Format(@"begin tran
+                                       UPDATE {0} with (serializable)
+                                       SET DataJson = @DataJson
+                                       WHERE Id = @Id
+                                       if @@ROWCOUNT = 0
+                                       begin
+                                          INSERT {0} (Id, DataJson)
+                                          VALUES (@Id,@DataJson)
+                                       end
+                                    commit tran", tableName);
+
+                using (var command = new SqlCommand(upsertSql))
                 {
                     command.Connection = sqlConnection;
-                    command.Parameters.Add("@CorrelationId", SqlDbType.UniqueIdentifier).Value = data.CorrelationId;
+                    command.Parameters.Add("@Id", SqlDbType.UniqueIdentifier).Value = data.CorrelationId;
+                    command.Parameters.Add("@DataJson", SqlDbType.Text).Value = sqlServerDataJson;
 
                     try
                     {
@@ -70,10 +114,24 @@ namespace R.MessageBus.Persistance.SqlServer
             throw new NotImplementedException();
         }
 
-        private static string GetTableName<T>(T data) where T : class, IProcessManagerData
+        private string GetTableName<T>(T data) where T : class, IProcessManagerData
         {
             Type typeParameterType = data.GetType();
             var tableName = typeParameterType.Name;
+
+            // Create table if doesn't exist
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
+                using (var command = new SqlCommand())
+                {
+                    command.Connection = connection;
+                    command.CommandText = string.Format("IF NOT EXISTS( SELECT 1 FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = '{0}') ", tableName) +
+                        string.Format("CREATE TABLE {0} (Id uniqueidentifier NOT NULL, DataJson text NULL);", tableName);
+                    command.ExecuteNonQuery();
+                }
+            }
+
             return tableName;
         }
     }
