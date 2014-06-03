eb81fac6bb30af74656648e0616df9944c7b7839
jpachansky <jpachansky@gmail.com>
2014-06-03 10:41:22 +0100

    Implement FindData and InsertData in SqlServerProcessManagerFinder
    Adding relevant integration test - using LocalDb instance.

--- src/R.MessageBus.IntegrationTests/SqlServerProcessManagerFinderTest.cs
+++ src/R.MessageBus.IntegrationTests/SqlServerProcessManagerFinderTest.cs
@@ -1,16 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using MongoDB.Driver.Builders;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
 using R.MessageBus.Interfaces;
-using R.MessageBus.Persistance.MongoDb;
 using R.MessageBus.Persistance.SqlServer;
 using Xunit;
 
 namespace R.MessageBus.IntegrationTests
 {
+    public class TestDbRow
+    {
+        public string Id { get; set; }
+        public string DataJson { get; set; }
+    }
+
     public class TestSqlServerData : IProcessManagerData
     {
         public Guid CorrelationId { get; set; }
@@ -24,7 +28,7 @@ namespace R.MessageBus.IntegrationTests
 
         public SqlServerProcessManagerFinderTest()
         {
-            _connectionString = "Server=localhost;Database=ProcessManagerRepository;Trusted_Connection=True;";
+            _connectionString = @"Data Source=(LocalDB)\v11.0;AttachDbFilename=|DataDirectory|\MyLocalDb.mdf;Integrated Security=True";
         }
 
         [Fact]
@@ -38,7 +42,97 @@ namespace R.MessageBus.IntegrationTests
             processManagerFinder.InsertData(data);
 
             // Assert
-            //todo
+            var results = GetTestDbData();
+            Assert.Equal(1, results.Count);
+            Assert.Equal(_correlationId.ToString(), results[0].Id);
+            Assert.True(results[0].DataJson.Contains("TestData"));
+        }
+
+        [Fact]
+        public void ShouldUpdateWhenInsertingDataWithExistingId()
+        {
+            // Arrange
+            SetupTestDbData(new List<TestDbRow> { new TestDbRow {Id = _correlationId.ToString(), DataJson = "FakeJsonData" }});
+
+            IProcessManagerData data = new TestSqlServerData { CorrelationId = _correlationId, Name = "TestData" };
+            IProcessManagerFinder processManagerFinder = new SqlServerProcessManagerFinder(_connectionString);
+
+            // Act
+            processManagerFinder.InsertData(data);
+
+            // Assert
+            var results = GetTestDbData();
+            Assert.Equal(1, results.Count);
+            Assert.Equal(_correlationId.ToString(), results[0].Id);
+            Assert.NotEqual("FakeJsonData", results[0].DataJson);
+            Assert.True(results[0].DataJson.Contains("TestData"));
+        }
+
+        [Fact]
+        public void ShouldFindData()
+        {
+            // Arrange
+            var testDataJson = "{\"CorrelationId\":\"e845f0a0-4af0-4d1e-a324-790d49d540ae\",\"Name\":\"TestData\"}";
+            SetupTestDbData(new List<TestDbRow> { new TestDbRow { Id = _correlationId.ToString(), DataJson = testDataJson } });
+            IProcessManagerFinder processManagerFinder = new SqlServerProcessManagerFinder(_connectionString);
+
+            // Act
+            var result = processManagerFinder.FindData<TestSqlServerData>(_correlationId);
+
+            // Assert
+            Assert.Equal("TestData", result.Data.Name);
+        }
+
+        private void SetupTestDbData(IEnumerable<TestDbRow> testData)
+        {
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
+
+                // Create table if doesn't exist
+                using (var command = new SqlCommand())
+                {
+                    command.Connection = connection;
+                    command.CommandText =
+                        "IF NOT EXISTS( SELECT 1 FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'TestSqlServerData') " +
+                        "CREATE TABLE TestSqlServerData(Id uniqueidentifier NOT NULL, DataJson text NULL);";
+                    command.ExecuteNonQuery();
+                }
+
+                foreach (var testDbRow in testData)
+                {
+                    using (var command = new SqlCommand())
+                    {
+                        command.Connection = connection;
+                        command.CommandText = @"INSERT TestSqlServerData (Id, DataJson) VALUES (@Id,@DataJson)";
+                        command.Parameters.Add("@Id", SqlDbType.UniqueIdentifier).Value = new Guid(testDbRow.Id);
+                        command.Parameters.Add("@DataJson", SqlDbType.Text).Value = testDbRow.DataJson;
+                        command.ExecuteNonQuery();
+                    }
+                }
+            }
+        }
+
+        private IList<TestDbRow> GetTestDbData()
+        {
+            IList<TestDbRow> results = new List<TestDbRow>();
+
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
+                using (var command = new SqlCommand())
+                {
+                    command.Connection = connection;
+                    command.CommandText = string.Format("SELECT * FROM TestSqlServerData WHERE Id = '{0}'", _correlationId.ToString());
+                    var reader = command.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        results.Add(new TestDbRow { Id = reader["Id"].ToString(), DataJson = reader["DataJson"].ToString() });
+                    }
+                }
+            }
+
+            return results;
         }
     }
 }
