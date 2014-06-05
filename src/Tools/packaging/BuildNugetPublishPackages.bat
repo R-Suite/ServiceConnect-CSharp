5bf3db9f7d30835cc384f49c47e500dad1362368
jpachansky <jpachansky@gmail.com>
2014-06-05 16:09:44 +0100

    Add constructor with required parameters to SqlServer ProcessManagerFinder

--- src/Tools/packaging/BuildNugetPublishPackages.bat
+++ src/Tools/packaging/BuildNugetPublishPackages.bat
@@ -14,7 +14,7 @@ NuGet pack "%OUTDIR%R.MessageBus\R.MessageBus.nuspec"
 :: NuGet pack "%OUTDIR%R.MessageBus.Persistance.MongoDb\R.MessageBus.Persistance.MongoDb.nuspec"
 
 
-nuget push R.MessageBus.1.0.1.13.nupkg
+nuget push R.MessageBus.1.0.1.14.nupkg
 :: nuget push R.MessageBus.Interfaces.1.0.1.12.nupkg
 :: nuget push R.MessageBus.Client.RabbitMQ.1.0.1.12.nupkg
 :: nuget push R.MessageBus.Persistance.MongoDb.1.0.1.12.nupkg
