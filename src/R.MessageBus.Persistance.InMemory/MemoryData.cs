1c0a3628ac9418b8d5f65a8fb7d1239dc03b7f92
jpachansky <jpachansky@gmail.com>
2014-06-05 15:32:17 +0100

    Remove dependency on MongoDb Client from the default R.MessageBus installation
    Using SqlServer implementation of ProccessManagerFinder by default. MongoDb implementation has been moved into a separate nuget package. R.MessageBus is not implicitly dependant on it.

---
+++ src/R.MessageBus.Persistance.InMemory/MemoryData.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using R.MessageBus.Interfaces;
+
+namespace R.MessageBus.Persistance.InMemory
+{
+    public class MemoryData<T> : IPersistanceData<T>
+    {
+        public Guid Id { get; set; }
+        public int Version { get; set; }
+
+        public T Data { get; set; }
+    }
+}
