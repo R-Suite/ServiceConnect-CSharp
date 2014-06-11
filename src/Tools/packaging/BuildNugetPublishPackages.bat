cec47cd1a173ec687bacf691796406c0e5a0577a
jpachansky <jpachansky@gmail.com>
2014-06-11 11:49:17 +0100

    Fix bug in exception serialization
    Top level exception message was not being included.

--- src/Tools/packaging/BuildNugetPublishPackages.bat
+++ src/Tools/packaging/BuildNugetPublishPackages.bat
@@ -11,13 +11,13 @@ del *.nupkg
 NuGet pack "%OUTDIR%R.MessageBus.Client.RabbitMQ\R.MessageBus.Client.RabbitMQ.nuspec"
 NuGet pack "%OUTDIR%R.MessageBus\R.MessageBus.nuspec"
 NuGet pack "%OUTDIR%R.MessageBus.Interfaces\R.MessageBus.Interfaces.nuspec"
-NuGet pack "%OUTDIR%R.MessageBus.Persistance.MongoDb\R.MessageBus.Persistance.MongoDb.nuspec"
+::NuGet pack "%OUTDIR%R.MessageBus.Persistance.MongoDb\R.MessageBus.Persistance.MongoDb.nuspec"
 
 
-nuget push R.MessageBus.1.0.1.15.nupkg
-nuget push R.MessageBus.Interfaces.1.0.1.13.nupkg
-nuget push R.MessageBus.Client.RabbitMQ.1.0.1.13.nupkg
-nuget push R.MessageBus.Persistance.MongoDb.1.0.1.13.nupkg
+nuget push R.MessageBus.1.0.1.16.nupkg
+nuget push R.MessageBus.Interfaces.1.0.1.14.nupkg
+nuget push R.MessageBus.Client.RabbitMQ.1.0.1.14.nupkg
+::nuget push R.MessageBus.Persistance.MongoDb.1.0.1.13.nupkg
            
 @ECHO === === === === === === === ===
 
