9faa271a4d0fda4426d8e2bda9cfd71365ca41bc
jpachansky <jpachansky@gmail.com>
2014-07-09 15:19:57 +0100

    Fix table locking issue in SqlServer Persister
    Using ROWLOCK hint (in addition UPDLOCK) top prevent lock on entire table.

--- src/Tools/packaging/BuildNugetPublishPackages.bat
+++ src/Tools/packaging/BuildNugetPublishPackages.bat
@@ -8,15 +8,15 @@ del *.nupkg
 
 :: comment
 
-NuGet pack "%OUTDIR%R.MessageBus.Client.RabbitMQ\R.MessageBus.Client.RabbitMQ.nuspec"
-::NuGet pack "%OUTDIR%R.MessageBus\R.MessageBus.nuspec"
+::NuGet pack "%OUTDIR%R.MessageBus.Client.RabbitMQ\R.MessageBus.Client.RabbitMQ.nuspec"
+NuGet pack "%OUTDIR%R.MessageBus\R.MessageBus.nuspec"
 ::NuGet pack "%OUTDIR%R.MessageBus.Interfaces\R.MessageBus.Interfaces.nuspec"
 ::NuGet pack "%OUTDIR%R.MessageBus.Persistance.MongoDb\R.MessageBus.Persistance.MongoDb.nuspec"
 
 
-::nuget push R.MessageBus.1.0.1.16.nupkg
+nuget push R.MessageBus.1.0.1.17.nupkg
 ::nuget push R.MessageBus.Interfaces.1.0.1.14.nupkg
-nuget push R.MessageBus.Client.RabbitMQ.1.0.1.16.nupkg
+::nuget push R.MessageBus.Client.RabbitMQ.1.0.1.16.nupkg
 ::nuget push R.MessageBus.Persistance.MongoDb.1.0.1.13.nupkg
            
 @ECHO === === === === === === === ===
