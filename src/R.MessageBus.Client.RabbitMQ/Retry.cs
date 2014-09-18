b2310bce496a502578ecedb300ceeadabfb691d7
twatson83 <tswatson123@gmail.com>
2014-09-18 13:21:39 +0100

    Added retry to rabbitmq consumer and producer when server disconnects

---
+++ src/R.MessageBus.Client.RabbitMQ/Retry.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace R.MessageBus.Client.RabbitMQ
+{
+    public static class Retry
+    {
+        public static void Do(Action action, Action<Exception> exceptionAction, TimeSpan retryInterval)
+        {
+            while (true)
+            {
+                try
+                {
+                    while (true)
+                    {
+                        action();
+                        return;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    exceptionAction(ex);
+                    Thread.Sleep(retryInterval);
+                }
+            }
+        }
+
+        public static void Do(Action action, Action<Exception> exceptionAction, TimeSpan retryInterval, int retryCount)
+        {
+            var exceptions = new List<Exception>();
+
+            for (int retry = 0; retry < retryCount; retry++)
+            {
+                try
+                {
+                    action();
+                    return;
+                }
+                catch (Exception ex)
+                {
+                    exceptions.Add(ex);
+                    exceptionAction(ex);
+                    Thread.Sleep(retryInterval);
+                }
+            }
+
+            throw new AggregateException(exceptions);
+        }
+
+        public static T Do<T>(Func<T> action, Action<Exception> exceptionAction, TimeSpan retryInterval, int retryCount)
+        {
+            var exceptions = new List<Exception>();
+
+            for (int retry = 0; retry < retryCount; retry++)
+            {
+                try
+                {
+                    return action();
+                }
+                catch (Exception ex)
+                {
+                    exceptions.Add(ex);
+                    exceptionAction(ex);
+                    Thread.Sleep(retryInterval);
+                }
+            }
+
+            throw new AggregateException(exceptions);
+        }
+    }
+}
