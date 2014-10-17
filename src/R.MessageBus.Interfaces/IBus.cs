4cde0f0737c2546b29941b64bd72ef4f3e666f46
jpachansky <jpachansky@gmail.com>
2014-10-17 13:10:52 +0100

    Add default timeout (3000) to synchronous SendRequest command

--- src/R.MessageBus.Interfaces/IBus.cs
+++ src/R.MessageBus.Interfaces/IBus.cs
@@ -3,7 +3,8 @@ using System;
 namespace R.MessageBus.Interfaces
 {
     public interface IBus : IDisposable
-    {   
+    {
+
         /// <summary>
         /// Contains the Bus configuration.
         /// </summary>
@@ -40,15 +41,16 @@ namespace R.MessageBus.Interfaces
         /// <param name="endPoint"></param>
         /// <param name="message"></param>
         void Send<T>(string endPoint, T message) where T : Message;
-       
+
         /// <summary>
         /// Sends a command and waits for a reply.  The method behaves like a regular blocking RPC method.
         /// </summary>
         /// <typeparam name="TRequest">The type of the request object.  Must be a message.</typeparam>
         /// <typeparam name="TReply">The type of the reply object. Must be a message.</typeparam>
         /// <param name="message">The message to send.</param>
+        /// <param name="timeout"></param>
         /// <returns>Returns the response object.</returns>
-        TReply SendRequest<TRequest, TReply>(TRequest message) where TRequest : Message where TReply : Message;
+        TReply SendRequest<TRequest, TReply>(TRequest message, int timeout = 3000) where TRequest : Message where TReply : Message;
 
         /// <summary>
         /// Sends a command to the specified endpoint and waits for a reply.  The method behaves like a regular blocking RPC method.
@@ -57,8 +59,9 @@ namespace R.MessageBus.Interfaces
         /// <typeparam name="TReply">The type of the reply object. Must be a message.</typeparam>
         /// <param name="endPoint">The endpoint the message will be sent to.</param>
         /// <param name="message">The message to send.</param>
+        /// <param name="timeout"></param>
         /// <returns>Returns the response object.</returns>
-        TReply SendRequest<TRequest, TReply>(string endPoint, TRequest message) where TRequest : Message where TReply : Message;
+        TReply SendRequest<TRequest, TReply>(string endPoint, TRequest message, int timeout = 3000) where TRequest : Message where TReply : Message;
 
         /// <summary>
         /// Sends a commands to the specified endpoint.  The callback is called when receving the reply message.
