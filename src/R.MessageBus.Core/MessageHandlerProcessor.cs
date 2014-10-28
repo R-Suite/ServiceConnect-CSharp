81aca41a3e66ce23cb6a986310ab622610b4a87d
jpachansky <jpachansky@gmail.com>
2014-10-28 18:38:54 +0000

    Fix bug with publishing and handling message base types

--- src/R.MessageBus.Core/MessageHandlerProcessor.cs
+++ src/R.MessageBus.Core/MessageHandlerProcessor.cs
@@ -26,23 +26,53 @@ namespace R.MessageBus.Core
                                                                         .Where(h => h.HandlerType.BaseType == null || 
                                                                                     h.HandlerType.BaseType.Name != typeof(ProcessManager<>).Name);
 
-            MethodInfo executeHandler = GetType().GetMethod("ExecuteHandler", BindingFlags.NonPublic | BindingFlags.Instance);
-            MethodInfo genericexecuteHandler = executeHandler.MakeGenericMethod(typeof(T));
+            InitHandlers<T>(message, context, handlerReferences);
+        }
+
+        private void ProcessMessageBaseType<T, TB>(string message, IConsumeContext context) where T : Message where TB : Message
+        {
+            IEnumerable<HandlerReference> handlerReferences = _container.GetHandlerTypes(typeof(IMessageHandler<TB>))
+                                                                        .Where(h => h.HandlerType.BaseType == null ||
+                                                                                    h.HandlerType.BaseType.Name != typeof(ProcessManager<>).Name);
+
+            InitHandlers<T>(message, context, handlerReferences, typeof(TB));
+        }        
+        
+        private void InitHandlers<T>(string message, IConsumeContext context, IEnumerable<HandlerReference> handlerReferences, Type baseType = null) where T : Message
+        {
+            string executeHandlerMethodName = (null != baseType) ? "ExecuteHandlerBaseType" : "ExecuteHandler";
+            MethodInfo executeHandler = GetType().GetMethod(executeHandlerMethodName, BindingFlags.NonPublic | BindingFlags.Instance);
+            MethodInfo genericexecuteHandler = (null != baseType) ? executeHandler.MakeGenericMethod(typeof(T), baseType) : executeHandler.MakeGenericMethod(typeof(T));
 
             foreach (HandlerReference handlerReference in handlerReferences)
             {
                 object messageObject = _messageSerializer.Deserialize(typeof (T).AssemblyQualifiedName, message);
-                genericexecuteHandler.Invoke(this, new[] { messageObject, handlerReference.HandlerType, context });
+                genericexecuteHandler.Invoke(this, new[] {messageObject, handlerReference.HandlerType, context});
             }
 
             // This is used when processing Sent (rather than Published) messages
             // Get message BaseType and call ProcessMessage recursively to see if there are any handlers interested in the BaseType
-            Type baseType = typeof(T).BaseType;
-            if (baseType != null && baseType.Name != typeof (Message).Name)
+            Type newBaseType = (null != baseType) ? baseType.BaseType : typeof(T).BaseType;
+            if (newBaseType != null && newBaseType.Name != typeof(Message).Name)
             {
-                MethodInfo processMessage = GetType().GetMethod("ProcessMessage", BindingFlags.Public | BindingFlags.Instance);
-                MethodInfo genericProcessMessage = processMessage.MakeGenericMethod(baseType);
-                genericProcessMessage.Invoke(this, new object[] { message, context });
+                MethodInfo processMessage = GetType().GetMethod("ProcessMessageBaseType", BindingFlags.NonPublic | BindingFlags.Instance);
+                MethodInfo genericProcessMessage = processMessage.MakeGenericMethod(typeof(T), newBaseType);
+                genericProcessMessage.Invoke(this, new object[] {message, context});
+            }
+        }
+
+        private void ExecuteHandlerBaseType<T, TB>(T message, Type handlerType, IConsumeContext context) where TB : Message where T : TB
+        {
+            try
+            {
+                var handler = (IMessageHandler<TB>)_container.GetInstance(handlerType);
+                handler.Context = context;
+                handler.Execute(message);
+            }
+            catch (Exception ex)
+            {
+                Logger.Error(string.Format("Error executing handler. {0}", handlerType.FullName), ex);
+                throw;
             }
         }
 
