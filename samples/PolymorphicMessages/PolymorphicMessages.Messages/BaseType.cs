0107b58a2e3a50c6f1e92bd745cc3a5e45d5744b
jpachansky <jpachansky@gmail.com>
2014-10-30 15:42:40 +0000

    Add sample project - PolymorphicMessages

---
+++ samples/PolymorphicMessages/PolymorphicMessages.Messages/BaseType.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using R.MessageBus.Interfaces;
+
+namespace PolymorphicMessages.Messages
+{
+    public class BaseType : Message
+    {
+        public BaseType(Guid correlationId) : base(correlationId)
+        {
+        }
+    }
+}
