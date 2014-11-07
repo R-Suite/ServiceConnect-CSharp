519d882120c199c7a8c293550c605dbed1eee743
twatson83 <tswatson123@gmail.com>
2014-11-07 16:31:48 +0000

    Add bus to container by default but add a flag to allow user to opt out

--- src/R.MessageBus.Interfaces/IBusContainer.cs
+++ src/R.MessageBus.Interfaces/IBusContainer.cs
@@ -13,5 +13,6 @@ namespace R.MessageBus.Interfaces
         void ScanForHandlers();
         void Initialize();
         void AddHandler<T>(Type handlerType, T handler);
+        void AddBus(IBus bus);
     }
 }
