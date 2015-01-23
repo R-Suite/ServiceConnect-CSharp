50c4a88a830ddc425df2dfcf0d53b367c4d645dc
jpachansky <jpachansky@gmail.com>
2015-01-23 15:03:10 +0000

    Add ability to configure how to find ProvessManager data. Including sample project.

---
+++ samples/ProcessManager/ProcessManager.Messages/StartProcessManagerMessage.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using R.MessageBus.Interfaces;
+
+namespace ProcessManager.Messages
+{
+    public class StartProcessManagerMessage : Message
+    {
+        public StartProcessManagerMessage(Guid correlationId) : base(correlationId)
+        {
+        }
+    }
+}
