43af4c7886a2c8e815809d73e68be02a1185d612
twatson83 <tswatson123@gmail.com>
2015-01-23 09:57:50 +0000

    Added IDisposable to producer and consumer interfaces

--- src/R.MessageBus.Interfaces/IProducer.cs
+++ src/R.MessageBus.Interfaces/IProducer.cs
@@ -1,14 +1,14 @@
+using System;
 using System.Collections.Generic;
 
 namespace R.MessageBus.Interfaces
 {
-    public interface IProducer
+    public interface IProducer : IDisposable
     {
         void Publish<T>(T message, Dictionary<string, string> headers = null) where T : Message;
         void Send<T>(T message, Dictionary<string, string> headers = null) where T : Message;
         void Send<T>(string endPoint, T message, Dictionary<string, string> headers = null) where T : Message;
         void Disconnect();
-        void Dispose();
         string Type { get;}
     }
 }
