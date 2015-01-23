d7822f82a52682e7fb5e6f32a06a16456206bf9d
twatson83 <tswatson123@gmail.com>
2015-01-23 09:57:50 +0000

    Added IDisposable to producer and consumer interfaces

--- src/R.MessageBus.Interfaces/IConsumer.cs
+++ src/R.MessageBus.Interfaces/IConsumer.cs
@@ -1,10 +1,11 @@
+using System;
+
 namespace R.MessageBus.Interfaces
 {
-    public interface IConsumer
+    public interface IConsumer : IDisposable
     {
         void StartConsuming(ConsumerEventHandler messageReceived, string queueName, bool? exclusive = null, bool? autoDelete = null);
         void StopConsuming();
-        void Dispose();
         void ConsumeMessageType(string messageTypeName);
         string Type { get; }
     }
