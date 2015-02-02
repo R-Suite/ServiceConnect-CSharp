dc6fe6a14c46eb0545c33cd103aa28909466aac3
twatson83 <tswatson123@gmail.com>
2015-02-02 09:44:55 +0000

    Added recipient list pattern.
    Can now specify a list of recipients when sending a command.  It is also
    possible to wait for responses from the endoints. If the request times out
    the method returns all responses recieved.

--- src/R.MessageBus.Interfaces/IBus.cs
+++ src/R.MessageBus.Interfaces/IBus.cs
@@ -53,7 +53,7 @@ namespace R.MessageBus.Interfaces
         /// <param name="endPoints"></param>
         /// <param name="message"></param>
         /// <param name="headers">Custom headers</param>
-        void Send<T>(IList<string> endPoints, T message, Dictionary<string, string> headers) where T : Message;
+        void Send<T>(IList<string> endPoints, T message, Dictionary<string, string> headers = null) where T : Message;
 
         /// <summary>
         /// Sends a command and waits for a reply.  The method behaves like a regular blocking RPC method.
@@ -79,6 +79,18 @@ namespace R.MessageBus.Interfaces
         TReply SendRequest<TRequest, TReply>(string endPoint, TRequest message, Dictionary<string, string> headers = null, int timeout = 3000) where TRequest : Message where TReply : Message;
 
         /// <summary>
+        /// Send a command to the specified endpoints and waits for all endpoints to reply. If all the endpoints dont respond in before the timeout then responses received are returned.
+        /// </summary>
+        /// <typeparam name="TRequest">The type of the request object.  Must be a message.</typeparam>
+        /// <typeparam name="TReply">The type of the reply object. Must be a message.</typeparam>
+        /// <param name="endPoints">The endpoints the message will be sent to.</param>
+        /// <param name="message">The message to send.</param>
+        /// <param name="timeout"></param>
+        /// <param name="headers">Custom headers</param>
+        /// <returns>Returns the response objects.</returns>
+        IList<TReply> SendRequest<TRequest, TReply>(IList<string> endPoints, TRequest message, Dictionary<string, string> headers = null, int timeout = 3000) where TRequest : Message where TReply : Message;
+
+        /// <summary>
         /// Sends a commands to the specified endpoint.  The callback is called when receving the reply message.
         /// </summary>
         /// <typeparam name="TRequest">The type of the request object.  Must be a message.</typeparam>
@@ -87,7 +99,6 @@ namespace R.MessageBus.Interfaces
         /// <param name="message">The message to send.</param>
         /// <param name="callback">The callback that will receive the response message.</param>
         /// <param name="headers">Custom headers</param>
-        /// <returns>Returns the response object.</returns>
         void SendRequest<TRequest, TReply>(string endPoint, TRequest message, Action<TReply> callback, Dictionary<string, string> headers = null) where TRequest : Message where TReply : Message;
 
         /// <summary>
@@ -98,8 +109,18 @@ namespace R.MessageBus.Interfaces
         /// <param name="message">The message to send.</param>
         /// <param name="callback">The callback that will receive the response message.</param>
         /// <param name="headers">Custom headers</param>
-        /// <returns>Returns the response object.</returns>
         void SendRequest<TRequest, TReply>(TRequest message, Action<TReply> callback, Dictionary<string, string> headers = null) where TRequest : Message where TReply : Message;
+
+        /// <summary>
+        /// Sends a commands to the specified endpoint.  The callback is called when receving the reply message.
+        /// </summary>
+        /// <typeparam name="TRequest">The type of the request object.  Must be a message.</typeparam>
+        /// <typeparam name="TReply">The type of the reply object. Must be a message.</typeparam>
+        /// <param name="endPoints">The endpoints the message will be sent to.</param>
+        /// <param name="message">The message to send.</param>
+        /// <param name="callback">The callback that will receive the response messages.</param>
+        /// <param name="headers">Custom headers</param>
+        void SendRequest<TRequest, TReply>(IList<string> endPoints, TRequest message, Action<IList<TReply>> callback, Dictionary<string, string> headers = null) where TRequest : Message where TReply : Message;
 
         /// <summary>
         /// Implementation of Routing Slip pattern. 
