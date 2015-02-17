0e5d56b2e37fcf1b4124a9b285cf9b9650a77e03
twatson83 <tswatson123@gmail.com>
2015-02-17 09:08:15 +0000

    Sample project for scatter gather

---
+++ samples/ScatterGather/ScatterGather.Consumer1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using R.MessageBus;
+
+namespace ScatterGather.Consumer1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*********** Consumer 1 ***********");
+            var bus = Bus.Initialize(x =>
+            {
+                x.ScanForMesssageHandlers = true;
+                x.SetQueueName("Consumer1");
+            });
+
+            bus.StartConsuming();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ samples/ScatterGather/ScatterGather.Messages/Request.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using R.MessageBus.Interfaces;
+
+namespace ScatterGather.Messages
+{
+    public class Request : Message
+    {
+        public Request(Guid correlationId) : base(correlationId)
+        {
+        }
+
+        public bool Delay { get; set; }
+    }
+}
