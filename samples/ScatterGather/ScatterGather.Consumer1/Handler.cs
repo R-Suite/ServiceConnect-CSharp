560286ea0d59f513d81c9f8d09340c68e7be3635
twatson83 <tswatson123@gmail.com>
2015-02-17 09:08:15 +0000

    Sample project for scatter gather

---
+++ samples/ScatterGather/ScatterGather.Consumer1/Handler.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading;
+using R.MessageBus.Interfaces;
+using ScatterGather.Messages;
+
+namespace ScatterGather.Consumer1
+{
+    public class Handler : IMessageHandler<Request>
+    {
+        public void Execute(Request message)
+        {
+            Console.WriteLine("Consumer 1 Received Message - {0}", message.CorrelationId);
+
+            if (message.Delay)
+            {
+                Thread.Sleep(1000);
+            }
+            
+            Context.Reply(new Response(message.CorrelationId)
+            {
+                Endpoint = "Consumer1"
+            });
+        }
+
+        public IConsumeContext Context { get; set; }
+    }
+}
---
+++ samples/ScatterGather/ScatterGather/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using R.MessageBus;
+using R.MessageBus.Interfaces;
+using ScatterGather.Messages;
+
+namespace ScatterGather
+{
+    class Program
+    {
+        private static IBus _bus;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*********** Publisher ***********");
+            _bus = Bus.Initialize(config =>
+            {
+
+            });
+            _bus.StartConsuming();
+
+            while (true)
+            {
+                Console.WriteLine("Choose a option");
+                Console.WriteLine("1 Scatter Gather Expect 2 replies");
+                Console.WriteLine("2 Scatter Gather unknown number of replies");
+
+                var result = Console.ReadLine();
+                switch (result)
+                {
+                    case "1":
+                        ScatterGatherKnown();
+                        break;
+                    case "2":
+                        ScatterGatherUnknown();
+                        break;
+                }
+            }
+        }
+
+        private static void ScatterGatherUnknown()
+        {
+            var id = Guid.NewGuid();
+            var responses = _bus.PublishRequest<Request, Response>(new Request(id){ Delay = true }, timeout: 500);
+
+            foreach (var response in responses)
+            {
+                Console.WriteLine("Received response from - {0}", response.Endpoint);
+            }
+
+            Console.WriteLine("");
+        }
+
+        private static void ScatterGatherKnown()
+        {
+            var id = Guid.NewGuid();
+            var responses = _bus.PublishRequest<Request, Response>(new Request(id), 2);
+
+            foreach (var response in responses)
+            {
+                Console.WriteLine("Received response from - {0}", response.Endpoint);
+            }
+
+            Console.WriteLine("");
+        }
+    }
+}
