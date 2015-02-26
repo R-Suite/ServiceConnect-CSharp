073b138c039e3c61d79351cbfc316c35fef9d2a1
twatson83 <tswatson123@gmail.com>
2015-02-26 11:42:34 +0000

    Added mongodb implementation of the aggregator persistor

---
+++ src/R.MessageBus.Persistance.MongoDb/MongoDBAggregatorPersistor.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using MongoDB.Driver;
+using MongoDB.Driver.Builders;
+using R.MessageBus.Interfaces;
+
+namespace R.MessageBus.Persistance.MongoDb
+{
+    public class MongoDBAggregatorPersistor : IAggregatorPersistor
+    {
+        private readonly MongoCollection<MongoDbData<object>> _collection;
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="connectionString"></param>
+        /// <param name="databaseName"></param>
+        public MongoDBAggregatorPersistor(string connectionString, string databaseName)
+        {
+            var mongoClient = new MongoClient(connectionString);
+            MongoServer server = mongoClient.GetServer();
+            var mongoDatabase = server.GetDatabase(databaseName);
+            _collection = mongoDatabase.GetCollection<MongoDbData<object>>("Aggregator");
+        }
+
+        public void InsertData(object data, string name)
+        {
+            _collection.Insert(new MongoDbData<object>
+            {
+                Name = name,
+                Data = data,
+                Version = 1
+            });
+        }
+
+        public IList<object> GetData(string name)
+        {
+            return _collection.Find(Query<MongoDbData<object>>.EQ(x => x.Name, name)).Select(x => x.Data).ToList();
+        }
+
+        public void RemoveAll(string name)
+        {
+            _collection.Remove(Query<MongoDbData<object>>.EQ(x => x.Name, name));
+        }
+
+        public int Count(string name)
+        {
+            return Convert.ToInt32(_collection.Count(Query<MongoDbData<object>>.EQ(x => x.Name, name)));
+        }
+    }
+}
