6b76c09c8a5ad546df87b37c57ac0468739f9515
jpachansky <jpachansky@gmail.com>
2015-02-27 15:18:16 +0000

    Update Aggregator timers
    Changed timers to reset each time a complete batch is dispatch to the relevant handlers.

--- src/R.MessageBus.Core/Aggregator.cs
+++ src/R.MessageBus.Core/Aggregator.cs
@@ -4,13 +4,27 @@ using R.MessageBus.Interfaces;
 
 namespace R.MessageBus.Core
 {
+    /// <summary>
+    /// Define aggregated message handlers
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
     public abstract class Aggregator<T> where T : Message
     {
+        /// <summary>
+        /// Timeout for aggregating messages.
+        /// When the timeout is reached, the current batch of messages is dispatched 
+        /// to the handler (regardless of the batch size).
+        /// </summary>
+        /// <returns></returns>
         public virtual TimeSpan Timeout()
         {
             return default(TimeSpan);
         }
 
+        /// <summary>
+        /// Max batch size of aggregated messages
+        /// </summary>
+        /// <returns></returns>
         public virtual int BatchSize()
         {
             return 0;
