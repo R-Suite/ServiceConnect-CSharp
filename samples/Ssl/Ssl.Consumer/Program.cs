845bc57bc30988c9399fe4a6e217f30462dc3a18
Tim Watson <twatson@ruffer.co.uk>
2015-06-05 09:06:51 +0100

    Moved git chat badge

---
+++ samples/Ssl/Ssl.Consumer/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Security;
+using System.Text;
+using System.Threading.Tasks;
+using R.MessageBus;
+using Ssl.Messages;
+
+namespace Ssl.Consumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var bus = Bus.Initialize(config =>
+            {
+                config.TransportSettings.SslEnabled = true;
+                config.TransportSettings.ServerName = "SslTest";
+                config.SetQueueName("Ssl.Consumer");
+                config.SetHost("lonappdev01");
+            });
+        }
+    }
+}
---
+++ samples/Ssl/Ssl/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using R.MessageBus;
+using Ssl.Messages;
+
+namespace Ssl.Producer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var bus = Bus.Initialize(config =>
+            {
+                config.TransportSettings.SslEnabled = true;
+                config.TransportSettings.ServerName = "SslTest";
+                config.SetQueueName("Ssl.Producer");
+                config.SetHost("lonappdev01");
+            });
+
+            bus.Send("Ssl.Consumer", new SslMessage(Guid.NewGuid()));
+        }
+    }
+}
