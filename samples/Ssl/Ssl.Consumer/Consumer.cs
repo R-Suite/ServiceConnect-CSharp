aa4285b74f6c1ba631e13f66382a7364cf201a3a
Tim Watson <twatson@ruffer.co.uk>
2015-06-05 09:06:51 +0100

    Moved git chat badge

---
+++ samples/Ssl/Ssl.Consumer/Consumer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using R.MessageBus.Interfaces;
+using Ssl.Messages;
+
+namespace Ssl.Consumer
+{
+    public class Consumer : IMessageHandler<SslMessage>
+    {
+        public void Execute(SslMessage message)
+        {
+            Console.WriteLine("Consumed message");
+        }
+
+        public IConsumeContext Context { get; set; }
+    }
+}
