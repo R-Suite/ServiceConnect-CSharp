f6641dd606ceee5da6771d5f63e2e5f2cd6fdd74
Tim Watson <twatson@ruffer.co.uk>
2015-06-10 11:45:58 +0100

    Added sample project for filters

---
+++ samples/Filters/Filters.Consumer/MessageHandler.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Filters.Messages;
+using R.MessageBus.Interfaces;
+
+namespace Filters.Consumer
+{
+    public class MessageHandler : IMessageHandler<FilterMessage>
+    {
+        public void Execute(FilterMessage message)
+        {
+            Console.WriteLine("Inside consumer - Value = " + message.FilterModifiedValue);
+        }
+
+        public IConsumeContext Context { get; set; }
+    }
+}
---
+++ samples/Filters/Filters.Consumer/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using R.MessageBus;
+
+namespace Filters.Consumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*********** Consumer ***********");
+            var bus = Bus.Initialize(config =>
+            {
+                config.SetQueueName("Filters.Consumer");
+                config.SetThreads(10);
+                config.BeforeFilters = new List<Type>
+                {
+                    typeof(BeforeFilter1),
+                    typeof(BeforeFilter2)
+                };
+                config.AfterFilters = new List<Type>
+                {
+                    typeof(AfterFilter1),
+                    typeof(AfterFilter2)
+                };
+                config.SetHost("lonappdev04");
+            });
+
+            bus.StartConsuming();
+
+            Console.ReadLine();
+        }
+    }
+}
