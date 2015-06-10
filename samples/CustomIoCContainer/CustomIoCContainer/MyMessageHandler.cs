04c1703bd9fef270c2cc2d1e5835b6ed5081b3c3
Jakub Pachansky <jpachansky@gmail.com>
2015-06-10 15:27:31 +0100

    Decouple IoC container from R.MessageBus
    R.MessageBus does not have a direct reference to StructureMap anymore. However, by default, R.MessageBus nuget will pull R.MessageBus.StructureMap nuget, which could be replaced by other (supported) containers in the future.  This is a breaking change.

---
+++ samples/CustomIoCContainer/CustomIoCContainer/MyMessageHandler.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using R.MessageBus.Interfaces;
+
+namespace CustomIoCContainer
+{
+    public class MyMessageHandler : IMessageHandler<MyMessage>
+    {
+        public void Execute(MyMessage message)
+        {
+            Console.Write("In MyMessageHandler. message.CorrelationId= {0}", message.CorrelationId);
+        }
+
+        public IConsumeContext Context { get; set; }
+    }
+}
