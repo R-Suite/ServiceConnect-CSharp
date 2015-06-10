3b74920d427cdccaca17f55bb67371a6452a9bcc
Tim Watson <twatson@ruffer.co.uk>
2015-06-10 12:15:11 +0100

    Added filters sample project

---
+++ samples/ProcessManager/ProcessManager.Messages/Filters.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using R.MessageBus.Interfaces;
+
+namespace ProcessManager.Messages
+{
+    public class Filter1 : IFilter
+    {
+        public bool Process(Envelope envelope)
+        {
+            Console.WriteLine("Filter1");
+            return true;
+        }
+    }
+
+    public class Filter2 : IFilter
+    {
+        public bool Process(Envelope envelope)
+        {
+            Console.WriteLine("Filter2");
+            return true;
+        }
+    }
+}
