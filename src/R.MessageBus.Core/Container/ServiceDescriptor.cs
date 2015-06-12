7e42c95452aee80096bd2a462d8824d465ef5217
Jakub Pachansky <jpachansky@gmail.com>
2015-06-12 18:11:53 +0100

    Remove dependency on StructureMap
    Using custom IoC by default. Supported containers will be available by nuget packages.

---
+++ src/R.MessageBus.Core/Container/ServiceDescriptor.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace R.MessageBus.Core.Container
+{
+    public class ServiceDescriptor
+    {
+        public Type ServiceType { get; set; }
+        public object Instance { get; set; }
+    }
+}
