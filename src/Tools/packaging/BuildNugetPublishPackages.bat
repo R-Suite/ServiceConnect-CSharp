96f046e3bbd103d7e386e2da03a06dcc89136764
Jakub Pachansky <jpachansky@gmail.com>
2015-06-15 12:02:44 +0100

    Fix bug in internal IoC
    Fixed bug with scanning for handlers where Aggragators wouldn't be found.
    By default, scan only assemblies loaded into the current AppDomain.

--- src/Tools/packaging/BuildNugetPublishPackages.bat
+++ src/Tools/packaging/BuildNugetPublishPackages.bat
@@ -1,4 +1,4 @@
-SET OUTDIR=C:\GitHub\R.MessageBus\src\
+SET OUTDIR=C:\Git\R.MessageBus\src\
 
 @ECHO === === === === === === === ===
 
@@ -8,16 +8,18 @@ del *.nupkg
 
 :: comment
 
-NuGet pack "%OUTDIR%R.MessageBus.Client.RabbitMQ\R.MessageBus.Client.RabbitMQ.nuspec"
+::NuGet pack "%OUTDIR%R.MessageBus.Client.RabbitMQ\R.MessageBus.Client.RabbitMQ.nuspec"
 NuGet pack "%OUTDIR%R.MessageBus\R.MessageBus.nuspec"
-NuGet pack "%OUTDIR%R.MessageBus.Interfaces\R.MessageBus.Interfaces.nuspec"
-NuGet pack "%OUTDIR%R.MessageBus.Persistance.MongoDb\R.MessageBus.Persistance.MongoDb.nuspec"
+::NuGet pack "%OUTDIR%R.MessageBus.Interfaces\R.MessageBus.Interfaces.nuspec"
+NuGet pack "%OUTDIR%R.MessageBus.Container.StructureMap\R.MessageBus.Container.StructureMap.nuspec"
+::NuGet pack "%OUTDIR%R.MessageBus.Persistance.MongoDb\R.MessageBus.Persistance.MongoDb.nuspec"
 
 
-nuget push R.MessageBus.1.1.25-beta.nupkg
-nuget push R.MessageBus.Interfaces.1.1.25-beta.nupkg
-nuget push R.MessageBus.Client.RabbitMQ.1.1.25-beta.nupkg
-nuget push R.MessageBus.Persistance.MongoDb.1.1.25-beta.nupkg
+nuget push R.MessageBus.2.1.0-beta.nupkg
+::nuget push R.MessageBus.Interfaces.2.1.0-beta.nupkg
+::nuget push R.MessageBus.Client.RabbitMQ.2.1.0-beta.nupkg
+nuget push R.MessageBus.Container.StructureMap.2.1.1-beta.nupkg
+::nuget push R.MessageBus.Persistance.MongoDb.1.1.25-beta.nupkg
            
 @ECHO === === === === === === === ===
 
