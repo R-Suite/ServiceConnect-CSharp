73da4b984ea59170aece2ab8de8489b548ec4a5b
Tim Watson <twatson@ruffer.co.uk>
2015-06-30 16:08:15 +0100

    Added GZip filter and sample project

---
+++ samples/GzipCompression/GzipCompression.Consumer/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using R.MessageBus;
+using R.MessageBus.Filters.GzipCompression;
+
+namespace GzipCompression.Consumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*********** GzipCompression Consumer ***********");
+            Bus.Initialize(x =>
+            {
+                x.SetHost("lonappdev04");
+                x.SetQueueName("GzipCompressionConsumer");
+                x.BeforeConsumingFilters = new List<Type>
+                {
+                    typeof(IncomingGzipCompressionFilter)
+                };
+            });
+            Console.ReadLine();
+        }
+    }
+}
