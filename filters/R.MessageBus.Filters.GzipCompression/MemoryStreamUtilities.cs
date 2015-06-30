2f402c36d11646d312c6b5f107f65430716e00ea
Tim Watson <twatson@ruffer.co.uk>
2015-06-30 16:08:15 +0100

    Added GZip filter and sample project

---
+++ filters/R.MessageBus.Filters.GzipCompression/MemoryStreamUtilities.cs
@@ -0,0 +1,19 @@
+﻿using System.IO;
+
+namespace R.MessageBus.Filters.GzipCompression
+{
+    public static class MemoryStreamUtilities
+    {
+        public static void CopyTo(Stream src, Stream dest)
+        {
+            var bytes = new byte[4096];
+
+            int cnt;
+
+            while ((cnt = src.Read(bytes, 0, bytes.Length)) != 0)
+            {
+                dest.Write(bytes, 0, cnt);
+            }
+        } 
+    }
+}
---
+++ filters/R.MessageBus.Filters.GzipCompression/OutgoingGzipCompressionFilter.cs
@@ -0,0 +1,24 @@
+﻿using System.IO;
+using System.IO.Compression;
+using R.MessageBus.Interfaces;
+
+namespace R.MessageBus.Filters.GzipCompression
+{
+    public class OutgoingGzipCompressionFilter : IFilter
+    {
+        public bool Process(Envelope envelope)
+        {
+            using (var messageMemoryStream = new MemoryStream(envelope.Body))
+            using (var compressedMessageMemoryStream = new MemoryStream())
+            {
+                using (var gzipStream = new GZipStream(compressedMessageMemoryStream, CompressionMode.Compress))
+                {
+                    MemoryStreamUtilities.CopyTo(messageMemoryStream, gzipStream);
+                }
+
+                envelope.Body = compressedMessageMemoryStream.ToArray();
+            }
+            return true;
+        }
+    }
+}
