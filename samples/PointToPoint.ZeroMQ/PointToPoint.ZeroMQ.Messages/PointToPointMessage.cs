9824446fbd93df49dda507663c73cb44172458e1
Jakub Pachansky <jpachansky@gmail.com>
2015-07-01 16:47:22 +0100

    Add [POC] ZeroMQ client
    Implemented "point-to-point" pattern and added a sample project.

---
+++ samples/PointToPoint.ZeroMQ/PointToPoint.ZeroMQ.Messages/PointToPointMessage.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using R.MessageBus.Interfaces;
+
+namespace PointToPoint.ZeroMQ.Messages
+{
+    public class PointToPointMessage : Message
+    {
+        public PointToPointMessage(Guid correlationId) : base(correlationId) { }
+    }
+}
