dbf50e5239c6ad77b65768d53a5718e4176ee205
Jakub Pachansky <jpachansky@gmail.com>
2015-07-01 16:47:22 +0100

    Add [POC] ZeroMQ client
    Implemented "point-to-point" pattern and added a sample project.

---
+++ src/R.MessageBus.Client.ZeroMQ/Consumer.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Newtonsoft.Json;
+using R.MessageBus.Interfaces;
+using ZeroMQ;
+
+namespace R.MessageBus.Client.ZeroMQ
+{
+    public class Consumer : IConsumer
+    {
+        private readonly string[] _hosts;
+        private int _activeHost;
+        private readonly ITransportSettings _transportSettings;
+
+        public Consumer(ITransportSettings transportSettings)
+        {
+            _transportSettings = transportSettings;
+
+            _hosts = transportSettings.Host.Split(',');
+            _activeHost = 0;
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void StartConsuming(ConsumerEventHandler messageReceived, string queueName, bool? exclusive = null, bool? autoDelete = null)
+        {
+            using (var context = new ZContext())
+            using (var responder = new ZSocket(context, ZSocketType.PULL))
+            {
+                // Bind
+                responder.Bind(_hosts[_activeHost]);
+
+                while (true)
+                {
+                    // Receive
+                    ZError error;
+                    ZMessage incoming;
+                    if (null == (incoming = responder.ReceiveMessage(out error)))
+                    {
+                        if (error == ZError.ETERM)
+                            return;    // Interrupted
+                        throw new ZException(error);
+                    }
+
+                    using (incoming)
+                    {
+                        var msg = new byte[incoming[1].Length];
+                        incoming[1].Read(msg, 0, (int) incoming[1].Length);
+
+                        IDictionary<string, object> headers = JsonConvert.DeserializeObject<Dictionary<string, object>>(incoming[0].ReadString());
+                        var typeName = headers["FullTypeName"].ToString();
+                        headers = headers.ToDictionary(k => k.Key, v => (object) Encoding.UTF8.GetBytes(v.Value.ToString()));
+
+                        messageReceived(msg, typeName, headers);
+                    }
+                }
+            }
+        }
+
+        public void StopConsuming()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ConsumeMessageType(string messageTypeName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Type { get; private set; }
+    }
+}
---
+++ src/R.MessageBus.Client.ZeroMQ/Producer.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Newtonsoft.Json;
+using R.MessageBus.Interfaces;
+using ZeroMQ;
+
+namespace R.MessageBus.Client.ZeroMQ
+{
+    public class Producer : IProducer
+    {
+        private readonly ITransportSettings _transportSettings;
+
+        public Producer(ITransportSettings transportSettings, IDictionary<string, IList<string>> queueMappings)
+        {
+            _transportSettings = transportSettings;
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Publish(string type, byte[] message, Dictionary<string, string> headers = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Send(string type, byte[] message, Dictionary<string, string> headers = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Send(string endPoint, string type, byte[] message, Dictionary<string, string> headers = null)
+        {
+            Dictionary<string, object> messageHeaders = GetHeaders(type, headers, endPoint, "Send");
+            var serializedHeaders = JsonConvert.SerializeObject(messageHeaders);
+
+            using (var context = new ZContext())
+            using (var requester = new ZSocket(context, ZSocketType.PUSH))
+            {
+                // Connect
+                requester.Connect(_transportSettings.Host);
+
+                var msg = new ZMessage();
+                msg.Append(new ZFrame(serializedHeaders));
+                msg.Append(new ZFrame(message));
+
+                // Send
+                requester.SendMessage(msg);
+            }
+        }
+
+        public void Disconnect()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Type { get; private set; }
+
+        public long MaximumMessageSize { get; private set; }
+
+        public void SendBytes(string endPoint, byte[] packet, Dictionary<string, string> headers)
+        {
+            throw new NotImplementedException();
+        }
+
+        private Dictionary<string, object> GetHeaders(string type, Dictionary<string, string> headers, string queueName, string messageType)
+        {
+            if (headers == null)
+            {
+                headers = new Dictionary<string, string>();
+            }
+
+            if (!headers.ContainsKey("DestinationAddress"))
+            {
+                headers["DestinationAddress"] = queueName;
+            }
+
+            if (!headers.ContainsKey("MessageId"))
+            {
+                headers["MessageId"] = Guid.NewGuid().ToString();
+            }
+
+            if (!headers.ContainsKey("MessageType"))
+            {
+                headers["MessageType"] = messageType;
+            }
+
+            headers["SourceAddress"] = _transportSettings.QueueName;
+            headers["TimeSent"] = DateTime.UtcNow.ToString("O");
+            headers["SourceMachine"] = _transportSettings.MachineName;
+            headers["FullTypeName"] = type;
+            headers["ConsumerType"] = "ZeroMQ";
+            headers["Language"] = "C#";
+
+            return headers.ToDictionary(x => x.Key, x => (object)x.Value);
+        }
+    }
+}
