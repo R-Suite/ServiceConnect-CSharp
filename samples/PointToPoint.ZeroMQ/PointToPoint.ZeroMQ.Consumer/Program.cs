41b4c2fc5c762ebf4fe5547398253c338685e359
Jakub Pachansky <jpachansky@gmail.com>
2015-07-01 16:47:22 +0100

    Add [POC] ZeroMQ client
    Implemented "point-to-point" pattern and added a sample project.

---
+++ samples/PointToPoint.ZeroMQ/PointToPoint.ZeroMQ.Consumer/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using R.MessageBus;
+using R.MessageBus.Core;
+
+namespace PointToPoint.ZeroMQ.Consumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*********** Consumer ***********");
+            var bus = Bus.Initialize(config =>
+            {
+                config.SetConsumer<R.MessageBus.Client.ZeroMQ.Consumer>();
+                config.SetProducer<R.MessageBus.Client.ZeroMQ.Producer>();
+                config.SetHost("tcp://127.0.0.1:5555");
+            });
+
+            Console.ReadLine();
+        }
+    }
+}
