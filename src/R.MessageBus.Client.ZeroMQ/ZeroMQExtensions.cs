3cf80467f542e8215d77b6648cad47f8611a912d
Jakub Pachansky <jpachansky@gmail.com>
2015-07-06 11:15:39 +0100

    Implement Point-To_point and PubSub pattern in ZeroMQ client
    Needs some sensible defaults and further refactoring.

---
+++ src/R.MessageBus.Client.ZeroMQ/ZeroMQExtensions.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using R.MessageBus.Interfaces;
+
+namespace R.MessageBus.Client.ZeroMQ
+{
+    public static class ZeroMQExtensions
+    {
+        public static void SetPublisherHost(this IConfiguration configuration, string host)
+        {
+            if (configuration.TransportSettings.ClientSettings == null)
+            {
+                configuration.TransportSettings.ClientSettings = new Dictionary<string, object>();
+            }
+            //todo: once we add defaults, check for existing key
+            configuration.TransportSettings.ClientSettings.Add("PublisherHost", host);
+        }
+
+        public static void SetSubscriberHost(this IConfiguration configuration, string host)
+        {
+            if (configuration.TransportSettings.ClientSettings == null)
+            {
+                configuration.TransportSettings.ClientSettings = new Dictionary<string, object>();
+            }
+            configuration.TransportSettings.ClientSettings.Add("SubscriberHost", host);
+        }
+
+        public static void SetSenderHost(this IConfiguration configuration, string host)
+        {
+            if (configuration.TransportSettings.ClientSettings == null)
+            {
+                configuration.TransportSettings.ClientSettings = new Dictionary<string, object>();
+            }
+            configuration.TransportSettings.ClientSettings.Add("SenderHost", host);
+        }
+
+        public static void SetReceiverHost(this IConfiguration configuration, string host)
+        {
+            if (configuration.TransportSettings.ClientSettings == null)
+            {
+                configuration.TransportSettings.ClientSettings = new Dictionary<string, object>();
+            }
+            configuration.TransportSettings.ClientSettings.Add("ReceiverHost", host);
+        }
+    }
+}
