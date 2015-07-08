0edde062351dce1fa487f2d73e4108ce70e34f60
Tim Watson <twatson@ruffer.co.uk>
2015-07-08 16:32:03 +0100

    Now pass type to prudcer so that we can set fullname and assembly qualified name in headers.  Now backwards compatible.

--- samples/PublishSubscribe/PublishSubscribe.Consumer1/Program.cs
+++ samples/PublishSubscribe/PublishSubscribe.Consumer1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Runtime.InteropServices;
 using R.MessageBus;
 
 namespace PublishSubscribe.Consumer1
@@ -7,11 +8,15 @@ namespace PublishSubscribe.Consumer1
     {
         static void Main(string[] args)
         {
+            var message = new PublishSubscribe.Messages.PublishSubscribeMessage(Guid.NewGuid());
+            var name = message.GetType().FullName;
+
             Console.WriteLine("*********** Consumer 1 ***********");
             var bus = Bus.Initialize(x =>
             {
                 x.ScanForMesssageHandlers = true;
                 x.SetQueueName("Consumer1");
+                x.SetHost("lonappdev04");
             });
 
             bus.StartConsuming();
