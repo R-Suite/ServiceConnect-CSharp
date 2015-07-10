4475c2b9b6f9bba344b8b8e58e7f5d5bdf5f281e
Jakub Pachansky <jpachansky@gmail.com>
2015-07-10 10:43:33 +0100

    Add retries, "audit" and "error" publishers to ZeroMQ PoC

--- src/R.MessageBus.Client.ZeroMQ/Consumer.cs
+++ src/R.MessageBus.Client.ZeroMQ/Consumer.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading;
+using Common.Logging;
 using Newtonsoft.Json;
 using R.MessageBus.Interfaces;
 using ZeroMQ;
@@ -11,17 +12,52 @@ namespace R.MessageBus.Client.ZeroMQ
 {
     public class Consumer : IConsumer
     {
+        private static readonly ILog Logger = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
         private readonly ITransportSettings _transportSettings;
         private ConsumerEventHandler _consumerEventHandler;
+        private readonly int _maxRetries;
+        private readonly bool _errorsDisabled;
+        private readonly ZContext _errorPublishContext;
+        private readonly ZSocket _errorPublisher;
+        private readonly ZContext _auditPublishContext;
+        private readonly ZSocket _auditPublisher;
 
         public Consumer(ITransportSettings transportSettings)
         {
             _transportSettings = transportSettings;
+            _maxRetries = transportSettings.MaxRetries;
+            _errorsDisabled = transportSettings.DisableErrors;
+
+            if (_transportSettings.ClientSettings.ContainsKey("ErrorPublisherHost"))
+            {
+                _errorPublishContext = new ZContext();
+                _errorPublisher = new ZSocket(_errorPublishContext, ZSocketType.PUB);
+                _errorPublisher.Linger = TimeSpan.FromMilliseconds(1);
+                _errorPublisher.Bind(_transportSettings.ClientSettings["ErrorPublisherHost"].ToString());
+            }
+
+            if (_transportSettings.ClientSettings.ContainsKey("AuditPublisherHost"))
+            {
+                _auditPublishContext = new ZContext();
+                _auditPublisher = new ZSocket(_auditPublishContext, ZSocketType.PUB);
+                _auditPublisher.Linger = TimeSpan.FromMilliseconds(1);
+                _auditPublisher.Bind(_transportSettings.ClientSettings["AuditPublisherHost"].ToString());
+            }
         }
 
         public void Dispose()
         {
-            throw new NotImplementedException();
+            if (null != _errorPublisher)
+            {
+                _errorPublisher.Dispose();
+                _errorPublishContext.Dispose();
+            }
+
+            if (null != _auditPublisher)
+            {
+                _auditPublisher.Dispose();
+                _auditPublishContext.Dispose();
+            }
         }
 
         public void StartConsuming(ConsumerEventHandler messageReceived, string queueName, bool? exclusive = null, bool? autoDelete = null)
@@ -54,15 +90,15 @@ namespace R.MessageBus.Client.ZeroMQ
 
                             using (incoming)
                             {
-                                var msg = new byte[incoming[1].Length];
-                                incoming[1].Read(msg, 0, (int) incoming[1].Length);
+                                var msgBody = new byte[incoming[1].Length];
+                                incoming[1].Read(msgBody, 0, (int) incoming[1].Length);
 
                                 IDictionary<string, object> headers =
                                     JsonConvert.DeserializeObject<Dictionary<string, object>>(incoming[0].ReadString());
                                 var typeName = (headers.ContainsKey("FullTypeName") ? headers["FullTypeName"] : headers["TypeName"]).ToString();
                                 headers = headers.ToDictionary(k => k.Key, v => (object) Encoding.UTF8.GetBytes(v.Value.ToString()));
 
-                                messageReceived(msg, typeName, headers);
+                                ProceesMessageRec(msgBody, typeName, headers);
                             }
                         }
                     }
@@ -101,8 +137,8 @@ namespace R.MessageBus.Client.ZeroMQ
 
                             using (incoming)
                             {
-                                var msg = new byte[incoming[2].Length];
-                                incoming[2].Read(msg, 0, (int) incoming[2].Length);
+                                var msgBody = new byte[incoming[2].Length];
+                                incoming[2].Read(msgBody, 0, (int)incoming[2].Length);
 
                                 IDictionary<string, object> headers =
                                     JsonConvert.DeserializeObject<Dictionary<string, object>>(incoming[1].ReadString());
@@ -113,11 +149,7 @@ namespace R.MessageBus.Client.ZeroMQ
                                 SetHeader(headers, "DestinationMachine", Environment.MachineName);
                                 SetHeader(headers, "DestinationAddress", _transportSettings.ClientSettings["SubscriberHost"].ToString());
 
-                                ConsumeEventResult result = _consumerEventHandler(msg, typeName, headers);
-
-                                if (!result.Success)
-                                {
-                                }
+                                ProceesMessageRec(msgBody, typeName, headers);
                             }
                         }
                     }
@@ -136,6 +168,96 @@ namespace R.MessageBus.Client.ZeroMQ
             else
             {
                 headers[key] = value;
+            }
+        }
+
+        private string GetErrorMessage(Exception exception)
+        {
+            var sbMessage = new StringBuilder();
+            sbMessage.Append(exception.Message + Environment.NewLine);
+            var ie = exception.InnerException;
+            while (ie != null)
+            {
+                sbMessage.Append(ie.Message + Environment.NewLine);
+                ie = ie.InnerException;
+            }
+
+            return sbMessage.ToString();
+        }
+
+        private void ProceesMessageRec(byte[] msgBody, string typeName, IDictionary<string, object> headers, int retryCount = 0)
+        {
+            ConsumeEventResult result = _consumerEventHandler(msgBody, typeName, headers);
+
+            if (!result.Success)
+            {
+                if (retryCount < _maxRetries)
+                {
+                    retryCount++;
+
+                    Thread.Sleep(_transportSettings.RetryDelay);
+
+                    ProceesMessageRec(msgBody, typeName, headers, retryCount);
+                }
+                else
+                {
+                    if (result.Exception != null)
+                    {
+                        string jsonException = string.Empty;
+                        try
+                        {
+                            jsonException = JsonConvert.SerializeObject(result.Exception);
+                        }
+                        catch (Exception ex)
+                        {
+                            Logger.Warn("Error serializing exception", ex);
+                        }
+
+                        SetHeader(headers, "Exception", JsonConvert.SerializeObject(new
+                        {
+                            TimeStamp = DateTime.Now,
+                            ExceptionType = result.Exception.GetType().FullName,
+                            Message = GetErrorMessage(result.Exception),
+                            result.Exception.StackTrace,
+                            result.Exception.Source,
+                            Exception = jsonException
+                        }));
+
+                        if (null != _errorPublisher)
+                        {
+                            var serializedHeaders = JsonConvert.SerializeObject(headers);
+
+                            var msg = new ZMessage();
+                            msg.Append(new ZFrame(_transportSettings.ErrorQueueName));
+                            msg.Append(new ZFrame(serializedHeaders));
+                            msg.Append(new ZFrame(msgBody));
+
+                            _auditPublisher.SendMessage(msg);
+                        }
+                    }
+
+                    Logger.ErrorFormat("Max number of retries exceeded. MessageId: {0}", headers["MessageId"]);
+                }
+            }
+            else if (!_errorsDisabled)
+            {
+                string messageType = null;
+                if (headers.ContainsKey("MessageType"))
+                {
+                    messageType = Encoding.UTF8.GetString((byte[])headers["MessageType"]);
+                }
+
+                if (_transportSettings.AuditingEnabled && messageType != "ByteStream" && null != _auditPublisher)
+                {
+                    var serializedHeaders = JsonConvert.SerializeObject(headers);
+
+                    var msg = new ZMessage();
+                    msg.Append(new ZFrame(_transportSettings.AuditQueueName));
+                    msg.Append(new ZFrame(serializedHeaders));
+                    msg.Append(new ZFrame(msgBody));
+
+                    _auditPublisher.SendMessage(msg);
+                }
             }
         }
     }
