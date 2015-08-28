f5b60a2dffed727a69ca93d29e0a8f074caaed17
Jakub Pachansky <jpachansky@gmail.com>
2015-08-28 11:52:33 +0100

    Refactor container extension methods
    Renamed InitializeContainer to SetContainer
    Renamed ConfigureContainer to ConfigureExistingContainer
    ConfigureExistingContainer creates a new container instance if it doesn't exist

--- src/R.MessageBus.Container.Ninject/NinjectExtensions.cs
+++ src/R.MessageBus.Container.Ninject/NinjectExtensions.cs
@@ -6,15 +6,27 @@ namespace R.MessageBus.Container.Ninject
 {
     public static class NinjectExtensions
     {
-        public static void InitializeContainer(this IConfiguration configuration, StandardKernel container)
+        /// <summary>
+        /// Initialize the bus with existing instance of Ninject Container.
+        /// </summary>
+        /// <param name="configuration"></param>
+        /// <param name="container"></param>
+        public static void SetContainer(this IConfiguration configuration, StandardKernel container)
         {
             configuration.SetContainerType<NinjectContainer>();
             var busContainer = configuration.GetContainer();
             busContainer.Initialize(container);
         }
 
-        public static void ConfigureContainer(this IConfiguration configuration, Action<StandardKernel> containerAction)
+        /// <summary>
+        /// Configure existing instance of Ninject Container. 
+        /// If existing instance does not exist, new one will be created.
+        /// </summary>
+        /// <param name="configuration"></param>
+        /// <param name="containerAction"></param>
+        public static void ConfigureExistingContainer(this IConfiguration configuration, Action<StandardKernel> containerAction)
         {
+            configuration.SetContainerType<NinjectContainer>();
             var busContainer = configuration.GetContainer();
             containerAction((StandardKernel)busContainer.GetContainer());
         }
--- src/Tools/packaging/BuildNugetPublishPackages.bat
+++ src/Tools/packaging/BuildNugetPublishPackages.bat
@@ -8,7 +8,7 @@ del *.nupkg
 
 :: comment
 
-::NuGet pack "%OUTDIR%R.MessageBus.Client.RabbitMQ\R.MessageBus.Client.RabbitMQ.nuspec"
+NuGet pack "%OUTDIR%R.MessageBus.Client.RabbitMQ\R.MessageBus.Client.RabbitMQ.nuspec"
 NuGet pack "%OUTDIR%R.MessageBus\R.MessageBus.nuspec"
 ::NuGet pack "%OUTDIR%R.MessageBus.Interfaces\R.MessageBus.Interfaces.nuspec"
 NuGet pack "%OUTDIR%R.MessageBus.Container.StructureMap\R.MessageBus.Container.StructureMap.nuspec"
@@ -16,12 +16,12 @@ NuGet pack "%OUTDIR%R.MessageBus.Container.StructureMap\R.MessageBus.Container.S
 ::NuGet pack "%OUTDIR%R.MessageBus.Container.Ninject\R.MessageBus.Container.Ninject.nuspec"
 
 
-nuget push R.MessageBus.2.1.4-beta.nupkg
-::nuget push R.MessageBus.Interfaces.2.1.1-beta.nupkg
-::nuget push R.MessageBus.Client.RabbitMQ.2.1.0-beta.nupkg
-nuget push R.MessageBus.Container.StructureMap.2.1.5-beta.nupkg
+nuget push R.MessageBus.2.1.6-beta.nupkg
+::nuget push R.MessageBus.Interfaces.2.1.2-beta.nupkg
+nuget push R.MessageBus.Client.RabbitMQ.2.1.2-beta.nupkg
+nuget push R.MessageBus.Container.StructureMap.2.1.7-beta.nupkg
 ::nuget push R.MessageBus.Persistance.MongoDb.1.1.25-beta.nupkg
-::nuget push R.MessageBus.Container.Ninject.2.1.2-beta.nupkg           
+::nuget push R.MessageBus.Container.Ninject.2.1.3-beta.nupkg           
 
 @ECHO === === === === === === === ===
 
