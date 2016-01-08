90fbaa43acd25f716b85ef7813edb5f5b65001a3
Jakub Pachansky <jpachansky@gmail.com>
2016-01-08 10:18:27 +0000

    Update VS ProjectTemplate

---
+++ samples/BusDisposeTest/BusDisposeTest/SmRegistry.cs
@@ -0,0 +1,17 @@
+﻿using StructureMap;
+using StructureMap.Configuration.DSL;
+
+namespace BusDisposeTest
+{
+    public class SmRegistry : Registry
+    {
+        public SmRegistry()
+        {
+            ObjectFactory.Configure(
+                cfg =>
+                {
+                    cfg.For<ITestClass>().Use<TestClass>();
+                });
+        }
+    }
+}
---
+++ samples/BusDisposeTest/BusDisposeTest/TestMsgHandler.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using BusDisposeTest.Messages;
+using log4net;
+using ServiceConnect.Interfaces;
+
+namespace BusDisposeTest
+{
+    public class TestMsgHandler : IMessageHandler<TestMsg>
+    {
+        private static readonly ILog Logger = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+
+        public void Execute(TestMsg message)
+        {
+            Logger.Info("in here");
+            string createText = "Hello and Welcome" + Environment.NewLine;
+            File.WriteAllText("C:\\GIT\\ServiceConnect\\samples\\BusDisposeTest\\BusDisposeTest\\bin\\Debug\\BusDisposeTest.txt", createText);
+        }
+
+        public IConsumeContext Context { get; set; }
+    }
+}
