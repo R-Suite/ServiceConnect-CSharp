7bceb5a421602860d3e2e3b9f2e659aa24291514
Jakub Pachansky <jpachansky@gmail.com>
2016-01-08 10:18:27 +0000

    Update VS ProjectTemplate

---
+++ samples/BusDisposeTest/BusDisposeTest/TestClass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace BusDisposeTest
+{
+    public class TestClass : ITestClass
+    {
+        public void Do()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    internal interface ITestClass
+    {
+        void Do();
+    }
+}
