a22ea710046e36ebfb6eae3e7f74eadb9a1c958f
Jakub Pachansky <jpachansky@gmail.com>
2016-03-15 09:43:44 +0000

    Add sample project and increase vesrsion
    Added sample project "CrossVersionSupport" ...demonstrating how to consume the old R.MessageBus messages and the new ServiceConnect messages in the same process.
    Increase package versions to 3.0.0.

---
+++ samples/CrossVersionSupport/CrossVersionSupport.Messages/ServiceConnectMessage.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using ServiceConnect.Interfaces;
+
+namespace CrossVersionSupport.Messages
+{
+    public class ServiceConnectMessage : Message
+    {
+        public ServiceConnectMessage(Guid correlationId) : base(correlationId)
+        {
+        }
+
+        public dynamic Test { get; set; }
+    }
+}
--- src/Tools/packaging/BuildNugetPublishPackages.bat
+++ src/Tools/packaging/BuildNugetPublishPackages.bat
@@ -8,20 +8,20 @@ del *.nupkg
 
 :: comment
 
-NuGet pack "%OUTDIR%ServiceConnect\ServiceConnect.nuspec"
+::NuGet pack "%OUTDIR%ServiceConnect\ServiceConnect.nuspec"
 NuGet pack "%OUTDIR%ServiceConnect.Client.RabbitMQ\ServiceConnect.Client.RabbitMQ.nuspec"
-NuGet pack "%OUTDIR%ServiceConnect.Interfaces\ServiceConnect.Interfaces.nuspec"
-NuGet pack "%OUTDIR%ServiceConnect.Container.StructureMap\ServiceConnect.Container.StructureMap.nuspec"
-NuGet pack "%OUTDIR%ServiceConnect.Persistance.MongoDb\ServiceConnect.Persistance.MongoDb.nuspec"
-NuGet pack "%OUTDIR%ServiceConnect.Container.Ninject\ServiceConnect.Container.Ninject.nuspec"
+::NuGet pack "%OUTDIR%ServiceConnect.Interfaces\ServiceConnect.Interfaces.nuspec"
+::NuGet pack "%OUTDIR%ServiceConnect.Container.StructureMap\ServiceConnect.Container.StructureMap.nuspec"
+::NuGet pack "%OUTDIR%ServiceConnect.Persistance.MongoDb\ServiceConnect.Persistance.MongoDb.nuspec"
+::NuGet pack "%OUTDIR%ServiceConnect.Container.Ninject\ServiceConnect.Container.Ninject.nuspec"
 
 
-nuget push ServiceConnect.2.1.14-beta.nupkg
-nuget push ServiceConnect.Client.RabbitMQ.2.1.8-beta.nupkg
-nuget push ServiceConnect.Interfaces.2.1.4-beta.nupkg
-nuget push ServiceConnect.Container.StructureMap.2.1.10-beta.nupkg
-nuget push ServiceConnect.Persistance.MongoDb.2.1.1-beta.nupkg
-nuget push ServiceConnect.Container.Ninject.2.1.5-beta.nupkg           
+::nuget push ServiceConnect.3.0.0.nupkg
+nuget push ServiceConnect.Client.RabbitMQ.3.0.0.nupkg
+::nuget push ServiceConnect.Interfaces.3.0.0.nupkg
+::nuget push ServiceConnect.Container.StructureMap.3.0.0.nupkg
+::nuget push ServiceConnect.Persistance.MongoDb.3.0.0.nupkg
+::nuget push ServiceConnect.Container.Ninject.3.0.0.nupkg           
 
 @ECHO === === === === === === === ===
 
