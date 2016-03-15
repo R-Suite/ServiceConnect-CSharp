c654672cf76e9f9c39db5773c99c01aac6a4226d
Jakub Pachansky <jpachansky@gmail.com>
2016-03-15 09:43:44 +0000

    Add sample project and increase vesrsion
    Added sample project "CrossVersionSupport" ...demonstrating how to consume the old R.MessageBus messages and the new ServiceConnect messages in the same process.
    Increase package versions to 3.0.0.

---
+++ samples/CrossVersionSupport/CrossVersionSupport.Consumer/ServiceConnectMessageHandler.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using CrossVersionSupport.Messages;
+using ServiceConnect.Interfaces;
+
+namespace CrossVersionSupport.Consumer
+{
+    public class ServiceConnectMessageHandler : IMessageHandler<ServiceConnectMessage>
+    {
+        public void Execute(ServiceConnectMessage message)
+        {
+            Console.WriteLine("ServiceConnectMessageHandler received message - {0}", message.Test);
+        }
+
+        public IConsumeContext Context { get; set; }
+    }
+}
