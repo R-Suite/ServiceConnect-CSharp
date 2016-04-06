952ff16614148d41cebd145af7d02ff51ccae317
Tim Watson <twatson@ruffer.co.uk>
2016-04-06 13:30:26 +0100

    Updated sample ssl proj

--- samples/Ssl/Ssl/Program.cs
+++ samples/Ssl/Ssl/Program.cs
@@ -11,13 +11,11 @@ namespace Ssl.Producer
             var bus = Bus.Initialize(config =>
             {
                 config.TransportSettings.SslEnabled = true;
-                config.TransportSettings.Password = "Pw)8dP8Vn]v<~n3M";
-                config.TransportSettings.Username = "admin";
-                config.TransportSettings.ServerName = "LONDATTST01";
-                config.TransportSettings.CertPassphrase = "6zbdsE8muh25yBgc";
-                config.TransportSettings.CertPath = "C:\\git\\Security\\1. Infrastructure\\c. RabbitMQ\\ssl\\client\\keycert.p12";
-                config.SetHost("LONDATTST01");
-                config.SetQueueName("Ssl.Producer");
+                config.TransportSettings.ServerName = "HOSTNAME";
+                config.TransportSettings.CertPassphrase = "client cert pass";
+                config.TransportSettings.CertPath = "keycert.p12";
+                config.SetQueueName("Ssl.Consumer");
+                config.ScanForMesssageHandlers = true;
 
             });
 
