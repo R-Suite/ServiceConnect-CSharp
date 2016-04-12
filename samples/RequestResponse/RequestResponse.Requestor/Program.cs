73026375ac564cdb57b1359dc6c359adad805224
Tim Watson <twatson@ruffer.co.uk>
2016-04-12 16:20:00 +0100

    Overloaded Reply method so that custom headers can be passed in a reply

--- samples/RequestResponse/RequestResponse.Requestor/Program.cs
+++ samples/RequestResponse/RequestResponse.Requestor/Program.cs
@@ -2,9 +2,24 @@
 using System.Collections.Generic;
 using ServiceConnect;
 using RequestRepsonse.Messages;
+using ServiceConnect.Interfaces;
 
 namespace RequestResponse.Requestor
 {
+    public class Filter : IFilter
+    {
+        public bool Process(Envelope envelope)
+        {
+            if (envelope.Headers.ContainsKey("Authenticated") && bool.Parse(System.Text.Encoding.ASCII.GetString((byte[])envelope.Headers["Authenticated"])))
+            {
+                Console.WriteLine("authenticated");
+                return true;
+            }
+            Console.WriteLine("not authenticated");
+            return false;
+        }
+    }
+
     class Program
     {
         private static void Main(string[] args)
@@ -14,6 +29,7 @@ namespace RequestResponse.Requestor
             var bus = Bus.Initialize(config =>
             {
                 config.SetQueueName("Requestor");
+                config.BeforeConsumingFilters.Add(typeof(Filter));
             });
             
             while (true)
@@ -21,13 +37,13 @@ namespace RequestResponse.Requestor
                 Console.WriteLine("Press enter to send messages");
                 Console.ReadLine();
 
-                var id = Guid.NewGuid();
-                Console.WriteLine("Sending synchronous message - {0}", id);
-                var result = bus.SendRequest<RequestMessage, ResponseMessage>("Responder", new RequestMessage(id), timeout: 300000);
-                Console.WriteLine("Sent synchronous message reply - {0}", result.CorrelationId);
-                Console.WriteLine();
+                //var id = Guid.NewGuid();
+                //Console.WriteLine("Sending synchronous message - {0}", id);
+                //var result = bus.SendRequest<RequestMessage, ResponseMessage>("Responder", new RequestMessage(id), timeout: 300000);
+                //Console.WriteLine("Sent synchronous message reply - {0}", result.CorrelationId);
+                //Console.WriteLine();
 
-                id = Guid.NewGuid();
+               var id = Guid.NewGuid();
                 Console.WriteLine("Sending async message - {0}", id);
                 bus.SendRequest<RequestMessage, ResponseMessage>("Responder", new RequestMessage(id), r => Console.WriteLine("Sent async message reply - {0}", r.CorrelationId));
                 Console.WriteLine();
