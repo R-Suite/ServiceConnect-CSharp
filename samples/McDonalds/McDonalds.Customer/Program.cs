3572b24fdd81f54a712f82044ffe550bb92bc1ae
Tim Watson <twatson@ruffer.co.uk>
2016-05-11 12:45:42 +0100

    Added MongoDB persistor with SSL support.

--- samples/McDonalds/McDonalds.Customer/Program.cs
+++ samples/McDonalds/McDonalds.Customer/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Security.Authentication;
 using McDonalds.Messages;
 using ServiceConnect;
 using ServiceConnect.Interfaces;
@@ -11,9 +12,16 @@ namespace McDonalds.Customer
         static void Main(string[] args)
         {
             Console.WriteLine("*********** Customer ***********");
-            IBus bus = Bus.Initialize(config =>
+            IBus bus = Bus.Initialize(x =>
             {
-                config.SetAuditingEnabled(true);
+                x.TransportSettings.SslEnabled = true;
+                x.TransportSettings.CertPassphrase = "secret";
+                x.TransportSettings.CertPath = "path";
+                x.TransportSettings.Username = "admin";
+                x.TransportSettings.Password = "password";
+                x.TransportSettings.ServerName = "node1,node2,node3";
+                x.TransportSettings.Version = SslProtocols.Default;
+                x.SetHost("node1,node2,node3");
             });
             bus.StartConsuming();
 
