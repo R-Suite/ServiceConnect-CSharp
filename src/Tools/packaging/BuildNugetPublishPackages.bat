1bff88554dc5f12f9ee2253749c6fbdec966cfd7
Jakub Pachansky <jpachansky@gmail.com>
2016-09-01 09:18:20 +0100

    Fix bug in message handling event
    Message headers must contain TypeName or FullTypeName, but not necessarily both.

--- src/Tools/packaging/BuildNugetPublishPackages.bat
+++ src/Tools/packaging/BuildNugetPublishPackages.bat
@@ -13,14 +13,16 @@ NuGet pack "%OUTDIR%ServiceConnect.Client.RabbitMQ\ServiceConnect.Client.RabbitM
 ::NuGet pack "%OUTDIR%ServiceConnect.Interfaces\ServiceConnect.Interfaces.nuspec"
 ::NuGet pack "%OUTDIR%ServiceConnect.Container.StructureMap\ServiceConnect.Container.StructureMap.nuspec"
 ::NuGet pack "%OUTDIR%ServiceConnect.Persistance.MongoDb\ServiceConnect.Persistance.MongoDb.nuspec"
+::NuGet pack "%OUTDIR%ServiceConnect.Persistance.MongoDbSsl\ServiceConnect.Persistance.MongoDbSsl.nuspec"
 ::NuGet pack "%OUTDIR%ServiceConnect.Container.Ninject\ServiceConnect.Container.Ninject.nuspec"
 
 
-::nuget push ServiceConnect.3.0.0.nupkg
-nuget push ServiceConnect.Client.RabbitMQ.3.0.0.nupkg
-::nuget push ServiceConnect.Interfaces.3.0.0.nupkg
+::nuget push ServiceConnect.3.0.2.nupkg -Source https://www.nuget.org/api/v2/package
+nuget push ServiceConnect.Client.RabbitMQ.3.0.4.nupkg -Source https://www.nuget.org/api/v2/package
+::nuget push ServiceConnect.Interfaces.3.0.2.nupkg -Source https://www.nuget.org/api/v2/package
 ::nuget push ServiceConnect.Container.StructureMap.3.0.0.nupkg
 ::nuget push ServiceConnect.Persistance.MongoDb.3.0.0.nupkg
+::nuget push ServiceConnect.Persistance.MongoDbSsl.3.0.1.nupkg -Source https://www.nuget.org/api/v2/package
 ::nuget push ServiceConnect.Container.Ninject.3.0.0.nupkg           
 
 @ECHO === === === === === === === ===
