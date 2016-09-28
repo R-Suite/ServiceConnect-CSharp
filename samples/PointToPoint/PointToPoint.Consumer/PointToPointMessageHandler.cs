b82b856f48f3526f5e50525da6622ef5669c3af0
Jakub Pachansky <jpachansky@gmail.com>
2016-09-28 14:41:12 +0100

    Fix bug with clients reconnecting to the broker after an unexpected shutdown
    Moved reconnecting logic from consumer shutdown event to the connection shutdown event.
    Also remove the experimental ZeroMQ client.

--- samples/PointToPoint/PointToPoint.Consumer/PointToPointMessageHandler.cs
+++ samples/PointToPoint/PointToPoint.Consumer/PointToPointMessageHandler.cs
@@ -1,4 +1,6 @@
-﻿using System.Threading;
+﻿using System;
+using System.IO;
+using System.Threading;
 using PointToPoint.Messages;
 using ServiceConnect.Interfaces;
 
@@ -8,7 +10,14 @@ namespace PointToPoint.Consumer
     {
         public void Execute(PointToPointMessage command)
         {
-            Thread.Sleep(100);
+            Thread.Sleep(400);
+            Console.WriteLine("{0}: Consumer 1 Received Message - {1}", Thread.CurrentThread.ManagedThreadId, command.CorrelationId);
+
+            using (StreamWriter writer =
+                new StreamWriter("check.txt", true))
+            {
+                writer.WriteLine("{0} - {1}", command.SerialNumber, command.CorrelationId);
+            }
         }
 
         public IConsumeContext Context { get; set; }
