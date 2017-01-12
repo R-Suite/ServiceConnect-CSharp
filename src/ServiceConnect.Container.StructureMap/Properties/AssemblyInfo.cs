40421ea649e780edb0752ea1c57f9c4ef570096f
Jakub Pachansky <jpachansky@gmail.com>
2017-01-12 13:00:24 +0000

    Convert ServiceConnect.StructureMap and unit test project

--- src/ServiceConnect.Container.StructureMap/Properties/AssemblyInfo.cs
+++ src/ServiceConnect.Container.StructureMap/Properties/AssemblyInfo.cs
@@ -2,35 +2,18 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
+// General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ServiceConnect.Container.StructureMap")]
-[assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Ruffer PLC")]
 [assembly: AssemblyProduct("ServiceConnect.Container.StructureMap")]
-[assembly: AssemblyCopyright("Copyright © ServiceConnect 2015")]
 [assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("d22d884e-16c0-49ba-8ea2-3d7b65df0a42")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: Guid("f93f32bd-f84c-4432-8887-c70a749bb6d3")]
