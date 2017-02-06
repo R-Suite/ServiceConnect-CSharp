e8a0dbe3bcc36c20452286600fd67cd106a1b5a2
Jakub Pachansky <jpachansky@gmail.com>
2017-02-06 14:43:44 +0000

    Fix bug with SmContainer not scanning all the assemblies

--- src/Tools/packaging_netcore/BuildNugetPublishPackages.bat
+++ src/Tools/packaging_netcore/BuildNugetPublishPackages.bat
@@ -13,17 +13,17 @@ del *.nupkg
 ::NuGet pack "%OUTDIR%ServiceConnect.Client.RabbitMQ\ServiceConnect.Client.RabbitMQ.nuspec"
 ::NuGet pack "%OUTDIR%ServiceConnect.Interfaces\ServiceConnect.Interfaces.nuspec"
 NuGet pack "%OUTDIR%ServiceConnect.Container.StructureMap\ServiceConnect.Container.StructureMap.nuspec
-NuGet pack "%OUTDIR%ServiceConnect.Persistance.MongoDb\ServiceConnect.Persistance.MongoDb.nuspec
-NuGet pack "%OUTDIR%ServiceConnect.Persistance.MongoDbSsl\ServiceConnect.Persistance.MongoDbSsl.nuspec
+::NuGet pack "%OUTDIR%ServiceConnect.Persistance.MongoDb\ServiceConnect.Persistance.MongoDb.nuspec
+::NuGet pack "%OUTDIR%ServiceConnect.Persistance.MongoDbSsl\ServiceConnect.Persistance.MongoDbSsl.nuspec
 ::NuGet pack "%OUTDIRFILTERS%ServiceConnect.Filters.MessageDeduplication\ServiceConnect.Filters.MessageDeduplication\ServiceConnect.Filters.MessageDeduplication.nuspec"
 ::NuGet pack "%OUTDIRFILTERS%ServiceConnect.Filters.GzipCompression\ServiceConnect.Filters.GzipCompression\ServiceConnect.Filters.GzipCompression.nuspec"
 
 ::nuget push ServiceConnect.4.0.2-pre.nupkg -Source https://www.nuget.org/api/v2/package
 ::nuget push ServiceConnect.Client.RabbitMQ.4.0.0-pre.nupkg -Source https://www.nuget.org/api/v2/package
 ::nuget push ServiceConnect.Interfaces.4.0.0-pre.nupkg -Source https://www.nuget.org/api/v2/package
-nuget push ServiceConnect.Container.StructureMap.4.0.2-pre.nupkg -Source https://www.nuget.org/api/v2/package
-nuget push ServiceConnect.Persistance.MongoDb.4.0.2-pre.nupkg -Source https://www.nuget.org/api/v2/package
-nuget push ServiceConnect.Persistance.MongoDbSsl.4.0.2-pre.nupkg -Source https://www.nuget.org/api/v2/package
+nuget push ServiceConnect.Container.StructureMap.4.0.3-pre.nupkg -Source https://www.nuget.org/api/v2/package
+::nuget push ServiceConnect.Persistance.MongoDb.4.0.2-pre.nupkg -Source https://www.nuget.org/api/v2/package
+::nuget push ServiceConnect.Persistance.MongoDbSsl.4.0.2-pre.nupkg -Source https://www.nuget.org/api/v2/package
 ::nuget push ServiceConnect.Filters.MessageDeduplication.2.0.2-pre.nupkg -Source https://www.nuget.org/api/v2/package
 ::nuget push ServiceConnect.Filters.GzipCompression.2.0.0-pre.nupkg -Source https://www.nuget.org/api/v2/package
 
