32fd7b4c0a7599d4d37740866e81f53d3e2bfc95
Jakub Pachansky <jpachansky@gmail.com>
2017-03-07 09:58:41 +0000

    Add Redis persistance option

---
+++ filters/ServiceConnect.Filters.MessageDeduplication/ServiceConnect.Filters.MessageDeduplication/Filters/IncomingDeduplicationFilterRedis.cs
@@ -0,0 +1,17 @@
+﻿using ServiceConnect.Filters.MessageDeduplication.Persistors;
+using ServiceConnect.Interfaces;
+
+namespace ServiceConnect.Filters.MessageDeduplication.Filters
+{
+    public class IncomingDeduplicationFilterRedis : IFilter
+    {
+        public bool Process(Envelope envelope)
+        {
+            var incomingFilter = new IncomingFilter(new MessageDeduplicationPersistorRedis());
+            
+            return incomingFilter.Process(envelope);
+        }
+
+        public IBus Bus { get; set; }
+    }
+}
