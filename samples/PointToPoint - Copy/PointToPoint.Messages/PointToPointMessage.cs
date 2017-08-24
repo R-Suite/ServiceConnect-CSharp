12ee78978c5cbeb8864f558bbe2b6a42fc45340d
Tim Watson <twatson@ruffer.co.uk>
2017-08-24 11:28:54 +0100

    * Added exception handling to Bus.checkstatus method * Added rabbitmq configuration options to specify retry count and retry seconds

---
+++ samples/PointToPoint - Copy/PointToPoint.Messages/PointToPointMessage.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using ServiceConnect.Interfaces;
+
+namespace PointToPoint.Messages
+{
+    public class PointToPointMessage : Message
+    {
+        public PointToPointMessage(Guid correlationId) : base(correlationId){}
+    }
+}
