8743217b0cbc2839a5155688c582607db2c10cb0
Jakub Pachansky <jpachansky@gmail.com>
2017-08-30 14:55:29 +0100

    Migrate project.json to csproj.

--- samples/NetCoreTest/src/NetCoreTest.Consumer/Program.cs
+++ samples/NetCoreTest/src/NetCoreTest.Consumer/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using ServiceConnect;
+using ServiceConnect.Container.Default;
 using ServiceConnect.Container.StructureMap;
 using StructureMap;
 
@@ -11,13 +12,15 @@ namespace NetCoreTest.Consumer
         {
             Console.WriteLine("*********** Consumer ***********");
 
-            IContainer myContainer = new StructureMap.Container();
+            //IContainer myContainer = new StructureMap.Container();
 
             var bus = Bus.Initialize(config =>
             {
-                config.SetContainer(myContainer);
+                //config.SetContainer(myContainer);
                 config.SetThreads(1);
                 //config.SetContainerType<DefaultBusContainer>();
+                config.SetContainerType<StructureMapContainer>();
+                config.ScanForMesssageHandlers = true;
                 config.SetHost("localhost");
                 config.TransportSettings.ClientSettings.Add("PrefetchCount", 7);
                 config.TransportSettings.ClientSettings.Add("HeartbeatEnabled", true);
