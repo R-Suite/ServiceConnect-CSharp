5ab7e0244044fd7ca5c2f0f669f95a7632e55032
Tim Watson <twatson@ruffer.co.uk>
2017-09-05 10:15:26 +0100

    Updated MongoDB and in memory process manager finder to retun null if finding property throws exception.  This can happen if looking for key in dictionary or if looking for a property on a nested object where the object is null.

--- src/ServiceConnect.Persistance.InMemory/InMemoryProcessManagerFinder.cs
+++ src/ServiceConnect.Persistance.InMemory/InMemoryProcessManagerFinder.cs
@@ -55,7 +55,17 @@ namespace ServiceConnect.Persistance.InMemory
                           mapper.Mappings.First(m => m.MessageType == typeof(Message));
 
 
-                object msgPropValue = mapping.MessageProp.Invoke(message);
+                object msgPropValue = null;
+
+                try
+                {
+                    msgPropValue = mapping.MessageProp.Invoke(message);
+                }
+                catch
+                {
+                    return null;
+                }
+                            
                 if (null == msgPropValue)
                 {
                     throw new ArgumentException("Message property expression evaluates to null");
