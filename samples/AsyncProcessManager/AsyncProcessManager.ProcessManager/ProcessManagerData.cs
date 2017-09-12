6111dcce2532e2bec60572a144c48361fbf8df17
Tim Watson <twatson@ruffer.co.uk>
2017-09-12 12:49:59 +0100

    Added sample project for async process manager handlers

---
+++ samples/AsyncProcessManager/AsyncProcessManager.ProcessManager/ProcessManagerData.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using ServiceConnect.Interfaces;
+
+namespace AsyncProcessManager.ProcessManager
+{
+    public class ProcessManagerData : IProcessManagerData
+    {
+        public Guid CorrelationId { get; set; }
+    }
+}
---
+++ samples/AsyncProcessManager/AsyncProcessManager.Producer/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AsyncProcessManager.Messages;
+using ServiceConnect;
+
+namespace AsyncProcessManager.Producer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*********** Producer ***********");
+            var bus = Bus.Initialize(config =>
+            {
+                config.AddQueueMapping(typeof(StartMessage), "AsyncProcessManager.ProcessManager");
+                config.SetHost("localhost");
+            });
+
+            Console.WriteLine("Press enter to send message");
+            while (true)
+            {
+                Console.ReadLine();
+                var id = Guid.NewGuid();
+                bus.Send(new StartMessage(id));
+                Console.WriteLine("Message sent");
+            }
+        }
+    }
+}
