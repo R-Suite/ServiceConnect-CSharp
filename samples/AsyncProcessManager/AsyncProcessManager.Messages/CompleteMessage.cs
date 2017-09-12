8b1add713fc828309cc975d46c35e9a6e0957d0e
Tim Watson <twatson@ruffer.co.uk>
2017-09-12 12:49:59 +0100

    Added sample project for async process manager handlers

---
+++ samples/AsyncProcessManager/AsyncProcessManager.Messages/CompleteMessage.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using ServiceConnect.Interfaces;
+
+namespace AsyncProcessManager.Messages
+{
+    public class CompleteMessage : Message
+    {
+        public CompleteMessage(Guid correlationId) : base(correlationId)
+        {
+        }
+    }
+}
