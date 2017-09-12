c975d45af9cebb482596f059c5720aa1451fa252
Tim Watson <twatson@ruffer.co.uk>
2017-09-12 12:49:59 +0100

    Added sample project for async process manager handlers

---
+++ samples/AsyncProcessManager/AsyncProcessManager.Producer/CompleteMessageHandler.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading.Tasks;
+using AsyncProcessManager.Messages;
+using ServiceConnect.Interfaces;
+
+namespace AsyncProcessManager.Producer
+{
+    public class CompleteMessageHandler : IAsyncMessageHandler<CompleteMessage>
+    {
+        public async Task Execute(CompleteMessage message)
+        {
+            await Task.Run(() =>
+            {
+                Console.WriteLine("Received process manager complete message");
+            });
+        }
+
+        public IConsumeContext Context { get; set; }
+    }
+}
