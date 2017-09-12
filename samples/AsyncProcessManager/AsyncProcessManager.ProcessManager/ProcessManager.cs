e3ac6ff92da4d614725971b7ca5caed7ea6d5eb1
Tim Watson <twatson@ruffer.co.uk>
2017-09-12 12:49:59 +0100

    Added sample project for async process manager handlers

---
+++ samples/AsyncProcessManager/AsyncProcessManager.ProcessManager/ProcessManager.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading.Tasks;
+using AsyncProcessManager.Messages;
+using ServiceConnect.Core;
+using ServiceConnect.Interfaces;
+
+namespace AsyncProcessManager.ProcessManager
+{
+    public class ProcessManagerHandler : ProcessManager<ProcessManagerData>, IStartAsyncProcessManager<StartMessage>, IAsyncMessageHandler<MessageResponse>
+    {
+        private readonly IBus _bus;
+
+        public ProcessManagerHandler(IBus bus)
+        {
+            _bus = bus;
+        }
+
+        public async Task Execute(StartMessage message)
+        {
+            await Task.Run(() =>
+            {
+                Data.CorrelationId = message.CorrelationId;
+                Console.WriteLine("Received start.");
+                _bus.Send("AsyncProcessManager.Consumer", new MessageRequest(message.CorrelationId));
+            });
+        }
+
+        public async Task Execute(MessageResponse message)
+        {
+            await Task.Run(() =>
+            {
+                Console.WriteLine("Received message response.");
+                _bus.Send("AsyncProcessManager.Producer", new CompleteMessage(message.CorrelationId));
+                MarkAsComplete();
+            });
+        }
+    }
+}
