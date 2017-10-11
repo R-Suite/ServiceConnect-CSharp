7f6125ac28305122e597e9dac00d96b1ab38cc13
Tim Watson <twatson@ruffer.co.uk>
2017-10-11 14:35:56 +0100

    Now fetch filters using container

--- samples/Filters/Filters/Filters.cs
+++ samples/Filters/Filters/Filters.cs
@@ -8,8 +8,16 @@ namespace Filters
 {
     public class Filter1 : IFilter
     {
+        private string _p1;
+
+        public Filter1(string p1)
+        {
+            _p1 = p1;
+        }
+
         public bool Process(Envelope envelope)
         {
+            Console.WriteLine(_p1);
             Console.WriteLine("Inside outgoing filter 1");
             var json = Encoding.UTF8.GetString(envelope.Body);
             var message = JsonConvert.DeserializeObject<FilterMessage>(json);
