ca8f3d5edca35bf3ab6ca35d5e357a98000d38ac
Jakub Pachansky <jpachansky@gmail.com>
2018-02-19 11:53:28 +0000

    Fix request-reply issue with netcore version

---
+++ samples/NetCoreRequestReply/NetCoreRequestReply/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NetCoreRequestReply
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
