2159f347b156464d310232c8117540e37fd4a92d
Jakub Pachansky <jpachansky@gmail.com>
2018-02-19 11:53:28 +0000

    Fix request-reply issue with netcore version

---
+++ samples/NetCoreRequestReply/RequestResponse.Requestor/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using RequestRepsonse.Messages;
+using ServiceConnect;
+
+namespace RequestResponse.Requestor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*********** Requestor ***********");
+
+            var bus = Bus.Initialize(config =>
+            {
+                config.SetQueueName("NetCoreRequestor");
+            });
+
+            while (true)
+            {
+                Console.WriteLine("Press enter to send messages");
+                Console.ReadLine();
+
+                var id = Guid.NewGuid();
+                Console.WriteLine("Sending async message - {0}", id);
+                bus.SendRequest<RequestMessage, ResponseMessage>("NetCoreResponder", new RequestMessage(id), r => Console.WriteLine("Sent async message reply - {0}", r.CorrelationId));
+                Console.WriteLine();
+            }
+        }
+    }
+}
