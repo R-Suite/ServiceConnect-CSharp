51fe3e2b61183f65602acda0163502c746c5ac4a
Jakub Pachansky <jpachansky@gmail.com>
2018-02-19 11:53:28 +0000

    Fix request-reply issue with netcore version

---
+++ samples/NetCoreRequestReply/RequestResponse.Responder/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using ServiceConnect;
+
+namespace RequestResponse.Responder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*********** Responder ***********");
+
+            Bus.Initialize(x =>
+            {
+                x.SetQueueName("NetCoreResponder");
+            });
+
+            Console.ReadLine();
+        }
+    }
+}
