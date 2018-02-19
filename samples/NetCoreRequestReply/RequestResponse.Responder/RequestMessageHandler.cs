23cafa373388be1d7e70e7400dcbc3170a9cae21
Jakub Pachansky <jpachansky@gmail.com>
2018-02-19 11:53:28 +0000

    Fix request-reply issue with netcore version

---
+++ samples/NetCoreRequestReply/RequestResponse.Responder/RequestMessageHandler.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using RequestRepsonse.Messages;
+using ServiceConnect.Interfaces;
+
+namespace RequestResponse.Responder
+{
+    public class RequestMessageHandler : IMessageHandler<RequestMessage>
+    {
+        public IConsumeContext Context { get; set; }
+
+        public void Execute(RequestMessage message)
+        {
+            Console.WriteLine("Received message, sending reply - {0}", message.CorrelationId);
+            Context.Reply(new ResponseMessage(message.CorrelationId), new Dictionary<string, string>
+            {
+                {"Authenticated", (DateTime.Now.Ticks % 2 == 0).ToString()}
+            });
+        }
+    }
+}
