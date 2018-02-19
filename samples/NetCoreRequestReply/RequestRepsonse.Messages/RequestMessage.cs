2d1e9c5eb7100efd19e058d311678b27a8bc9764
Jakub Pachansky <jpachansky@gmail.com>
2018-02-19 11:53:28 +0000

    Fix request-reply issue with netcore version

---
+++ samples/NetCoreRequestReply/RequestRepsonse.Messages/RequestMessage.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using ServiceConnect.Interfaces;
+
+namespace RequestRepsonse.Messages
+{
+    public class RequestMessage : Message
+    {
+        public RequestMessage(Guid correlationId) : base(correlationId)
+        {
+        }
+    }
+}
---
+++ samples/NetCoreRequestReply/RequestRepsonse.Messages/ResponseMessage.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using ServiceConnect.Interfaces;
+
+namespace RequestRepsonse.Messages
+{
+    public class ResponseMessage : Message
+    {
+        public ResponseMessage(Guid correlationId) : base(correlationId)
+        {
+        }
+    }
+}
