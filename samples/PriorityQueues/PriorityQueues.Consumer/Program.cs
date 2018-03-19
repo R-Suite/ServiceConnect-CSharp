c89d43ce77c8b23ae66bb5de96f38accc1bc0c1f
Jakub Pachansky <jpachansky@gmail.com>
2018-03-19 15:20:39 +0000

    Add support for priority queues

---
+++ samples/PriorityQueues/PriorityQueues.Consumer/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using ServiceConnect;
+using ServiceConnect.Container.StructureMap;
+
+namespace PriorityQueues.Consumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*********** Consumer ***********");
+
+            IDictionary<string, object> csArgs = new Dictionary<string, object> {{"x-max-priority", (int)10 }};
+
+            var bus = Bus.Initialize(config =>
+            {
+                config.SetContainerType<StructureMapContainer>();
+                config.SetThreads(1);
+                config.TransportSettings.ClientSettings.Add("Arguments", csArgs);
+                config.SetErrorQueueName("PriorityQueues.Consumer.Errors");
+            });
+
+            Console.ReadLine();
+
+            bus.Dispose();
+        }
+    }
+}
