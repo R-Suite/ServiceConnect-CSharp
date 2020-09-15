d0a85110a36dac4e46291beb4e0e0aa760d7a6d3
Jakub Pachansky <jPachansky@Ruffer.co.uk>
2020-09-15 15:57:52 +0100

    Fix issue with parsing connectionstring in MongoDBSslAggregatorPersistor

--- src/Tools/packaging_netcore/BuildNugetPublishPackages.bat
+++ src/Tools/packaging_netcore/BuildNugetPublishPackages.bat
@@ -23,9 +23,9 @@ NuGet pack "%OUTDIR%ServiceConnect.Persistance.MongoDbSsl\ServiceConnect.Persist
 ::nuget push ServiceConnect.Client.RabbitMQ.5.0.8.nupkg -Source https://www.nuget.org/api/v2/package
 ::nuget push ServiceConnect.Interfaces.5.0.4.nupkg -Source https://www.nuget.org/api/v2/package
 ::nuget push ServiceConnect.Container.StructureMap.5.0.1.nupkg -Source https://www.nuget.org/api/v2/package
-::nuget push ServiceConnect.Container.ServiceCollection.1.0.3.nupkg -Source https://www.nuget.org/api/v2/package
+::nuget push ServiceConnect.Container.ServiceCollection.1.0.4.nupkg -Source https://www.nuget.org/api/v2/package
 ::nuget push ServiceConnect.Persistance.MongoDb.5.0.2.nupkg -Source https://www.nuget.org/api/v2/package
-nuget push ServiceConnect.Persistance.MongoDbSsl.6.0.1.nupkg -Source https://www.nuget.org/api/v2/package
+nuget push ServiceConnect.Persistance.MongoDbSsl.6.0.2.nupkg -Source https://www.nuget.org/api/v2/package
 ::nuget push ServiceConnect.Filters.MessageDeduplication.2.0.6.nupkg -Source https://www.nuget.org/api/v2/package
 ::nuget push ServiceConnect.Filters.GzipCompression.2.0.0-pre.nupkg -Source https://www.nuget.org/api/v2/package
 
