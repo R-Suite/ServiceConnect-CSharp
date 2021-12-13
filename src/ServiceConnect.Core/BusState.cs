3adc4b6803ad47ed52f9c7b90b54d3d394c77b36
Timothy Watson <twatson@Ruffer.co.uk>
2021-12-13 16:21:43 +0000

    Increased versions and changed syntax to support .net 451

--- src/ServiceConnect.Core/BusState.cs
+++ src/ServiceConnect.Core/BusState.cs
@@ -1,16 +1,24 @@
 ﻿using ServiceConnect.Interfaces;
 using System;
 using System.Collections.Generic;
-using System.Text;
 
 namespace ServiceConnect.Core
 {
     public class BusState : IBusState
     {
-        public IDictionary<string, IRequestConfiguration> RequestConfigurations { get; set; } = new Dictionary<string, IRequestConfiguration>();
-        public IDictionary<string, IMessageBusReadStream> ByteStreams { get; set; } = new Dictionary<string, IMessageBusReadStream>();
-        public object RequestLock { get; set; } = new object();
-        public object ByteStreamLock { get; set; } = new object();
-        public IDictionary<Type, IAggregatorProcessor> AggregatorProcessors { get; set; } = new Dictionary<Type, IAggregatorProcessor>();
+        public IDictionary<string, IRequestConfiguration> RequestConfigurations { get; set; }
+        public IDictionary<string, IMessageBusReadStream> ByteStreams { get; set; }
+        public object RequestLock { get; set; }
+        public object ByteStreamLock { get; set; }
+        public IDictionary<Type, IAggregatorProcessor> AggregatorProcessors { get; set; }
+
+        public BusState()
+        {
+            RequestConfigurations = new Dictionary<string, IRequestConfiguration>();
+            ByteStreams = new Dictionary<string, IMessageBusReadStream>();
+            RequestLock = new object();
+            ByteStreamLock = new object();
+            AggregatorProcessors = new Dictionary<Type, IAggregatorProcessor>();
+        }
     }
 }
