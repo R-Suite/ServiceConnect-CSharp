834f84c38176a579a13ac653b7b89a70f038e525
Tim Watson <tswatson123@gmail.com>
2026-04-13 00:28:58 +0100

    refactor: collapse 8 filter variants into 2 generic classes with PersistorFactory (R-022)

--- filters/ServiceConnect.Filters.MessageDeduplication/ServiceConnect.Filters.MessageDeduplication/Filters/IncomingDeduplicationFilter.cs
+++ filters/ServiceConnect.Filters.MessageDeduplication/ServiceConnect.Filters.MessageDeduplication/Filters/IncomingDeduplicationFilter.cs
@@ -1,19 +1,18 @@
-﻿using System;
-using ServiceConnect.Filters.MessageDeduplication.Persistors;
+using System;
 using ServiceConnect.Interfaces;
 
 namespace ServiceConnect.Filters.MessageDeduplication.Filters
 {
-    public class IncomingDeduplicationFilterMongoDb : IFilter
+    public class IncomingDeduplicationFilter : IFilter
     {
         private static readonly Lazy<IncomingFilter> _incomingFilter = new(() =>
-            new IncomingFilter(new MessageDeduplicationPersistorMongoDb()));
+            new IncomingFilter(PersistorFactory.Create(DeduplicationFilterSettings.Instance.PersistorType)));
+
+        public IBus Bus { get; set; }
 
         public bool Process(Envelope envelope)
         {
             return _incomingFilter.Value.Process(envelope);
         }
-
-        public IBus Bus { get; set; }
     }
 }
--- filters/ServiceConnect.Filters.MessageDeduplication/ServiceConnect.Filters.MessageDeduplication/Filters/OutgoingDeduplicationFilter.cs
+++ filters/ServiceConnect.Filters.MessageDeduplication/ServiceConnect.Filters.MessageDeduplication/Filters/OutgoingDeduplicationFilter.cs
@@ -1,19 +1,18 @@
-﻿using System;
-using ServiceConnect.Filters.MessageDeduplication.Persistors;
+using System;
 using ServiceConnect.Interfaces;
 
 namespace ServiceConnect.Filters.MessageDeduplication.Filters
 {
-    public class OutgoingDeduplicationFilterMongoDb : IFilter
+    public class OutgoingDeduplicationFilter : IFilter
     {
         private static readonly Lazy<OutgoingFilter> _outgoingFilter = new(() =>
-            new OutgoingFilter(new MessageDeduplicationPersistorMongoDb()));
+            new OutgoingFilter(PersistorFactory.Create(DeduplicationFilterSettings.Instance.PersistorType)));
+
+        public IBus Bus { get; set; }
 
         public bool Process(Envelope envelope)
         {
             return _outgoingFilter.Value.Process(envelope);
         }
-
-        public IBus Bus { get; set; }
     }
 }
