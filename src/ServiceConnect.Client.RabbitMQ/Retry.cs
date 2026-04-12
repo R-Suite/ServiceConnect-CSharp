d38fadd93d5becdabff44298cce33ffe8b01ae27
Tim Watson <tswatson123@gmail.com>
2026-04-12 22:38:49 +0100

    fix: use Random.Shared for thread-safe jitter in Retry (C-06)

--- src/ServiceConnect.Client.RabbitMQ/Retry.cs
+++ src/ServiceConnect.Client.RabbitMQ/Retry.cs
@@ -2,8 +2,6 @@ namespace ServiceConnect.Client.RabbitMQ;
 
 public static class Retry
 {
-    private static readonly Random Jitter = new();
-
     public static async Task DoAsync(Func<Task> action, Func<Exception, Task> exceptionAction, TimeSpan retryInterval, int retryCount)
     {
         List<Exception> exceptions = [];
@@ -68,7 +66,7 @@ public static class Retry
     private static TimeSpan CalculateDelay(TimeSpan baseInterval, int retryAttempt)
     {
         var exponentialDelay = TimeSpan.FromMilliseconds(baseInterval.TotalMilliseconds * Math.Pow(2, retryAttempt));
-        var jitterMs = Jitter.Next(0, (int)Math.Min(baseInterval.TotalMilliseconds, 1000));
+        var jitterMs = Random.Shared.Next(0, (int)Math.Min(baseInterval.TotalMilliseconds, 1000));
         var totalDelay = exponentialDelay + TimeSpan.FromMilliseconds(jitterMs);
         return TimeSpan.FromMilliseconds(Math.Min(totalDelay.TotalMilliseconds, TimeSpan.FromMinutes(5).TotalMilliseconds));
     }
