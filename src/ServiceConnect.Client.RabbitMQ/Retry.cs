5a19c704425f2a43f7cafdacfb491515dc558907
Tim Watson <tswatson123@gmail.com>
2026-04-12 19:27:47 +0100

    feat: add exponential backoff with jitter to Retry (R-079)

--- src/ServiceConnect.Client.RabbitMQ/Retry.cs
+++ src/ServiceConnect.Client.RabbitMQ/Retry.cs
@@ -2,6 +2,8 @@ namespace ServiceConnect.Client.RabbitMQ;
 
 public static class Retry
 {
+    private static readonly Random Jitter = new();
+
     public static async Task DoAsync(Func<Task> action, Func<Exception, Task> exceptionAction, TimeSpan retryInterval, int retryCount)
     {
         List<Exception> exceptions = [];
@@ -24,7 +26,9 @@ public static class Retry
                 {
                     exceptions.Add(callbackEx);
                 }
-                await Task.Delay(retryInterval).ConfigureAwait(false);
+
+                var delay = CalculateDelay(retryInterval, retry);
+                await Task.Delay(delay).ConfigureAwait(false);
             }
         }
 
@@ -52,10 +56,20 @@ public static class Retry
                 {
                     exceptions.Add(callbackEx);
                 }
-                await Task.Delay(retryInterval).ConfigureAwait(false);
+
+                var delay = CalculateDelay(retryInterval, retry);
+                await Task.Delay(delay).ConfigureAwait(false);
             }
         }
 
         throw new AggregateException(exceptions);
+    }
+
+    private static TimeSpan CalculateDelay(TimeSpan baseInterval, int retryAttempt)
+    {
+        var exponentialDelay = TimeSpan.FromMilliseconds(baseInterval.TotalMilliseconds * Math.Pow(2, retryAttempt));
+        var jitterMs = Jitter.Next(0, (int)Math.Min(baseInterval.TotalMilliseconds, 1000));
+        var totalDelay = exponentialDelay + TimeSpan.FromMilliseconds(jitterMs);
+        return TimeSpan.FromMilliseconds(Math.Min(totalDelay.TotalMilliseconds, TimeSpan.FromMinutes(5).TotalMilliseconds));
     }
 }
