15977f92d2226404d730d68f1dc55f9aba6b5dc7
Tim Watson <tswatson123@gmail.com>
2026-04-29 09:32:49 +0100

    build: remove examples/MessageDeduplication

--- examples/MessageDeduplication/src/ServiceConnect.Examples.MessageDeduplication.Contracts/OrderPlaced.cs
+++
@@ -1,9 +0,0 @@
-using ServiceConnect.Interfaces;
-
-namespace ServiceConnect.Examples.MessageDeduplication.Contracts;
-
-public sealed class OrderPlaced(Guid correlationId) : Message(correlationId)
-{
-    public string OrderId { get; init; } = string.Empty;
-    public decimal Total { get; init; }
-}
